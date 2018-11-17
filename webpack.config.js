9e412b8d131e352b3730441fcfc8aa8ed8009239
Joonas Kallunki <jkallunki@gmail.com>
2018-11-17 08:25:03 +0200

    Configure Typescript build with Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'source-map',
+module.exports = env => ({
+  entry: './src/index.tsx',
+  mode: env.production ? 'production' : 'development',
+  devtool: env.production ? 'inline-source-map' : 'eval',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'suomifi-ui-components',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [
+      // Run the typescript compilier on .ts files before webpack
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
       }
     ]
+  },
+  externals: {
+    // this line is just to use the React dependency of our parent-project instead of using our own React.
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
   }
-};
+});
