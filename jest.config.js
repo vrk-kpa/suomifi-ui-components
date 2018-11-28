475a52faa2c47090c25635936ebd8438b4827e33
Joonas Kallunki <jkallunki@gmail.com>
2018-11-28 06:29:03 +0200

    Replace Mocha with Jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  preset: 'ts-jest',
+
+  setupTestFrameworkScriptFile: require.resolve('./jest.setup.js')
+};
