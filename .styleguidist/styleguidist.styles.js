fa361cae43995b2526a8e81dd1e40c2afa281421
Joonas Kallunki <jkallunki@gmail.com>
2018-12-19 14:23:00 +0200

    Separate styles from styleguidist config

---
+++ .styleguidist/styleguidist.styles.js
@@ -0,0 +1,67 @@
+module.exports = {
+  Logo: {
+    logo: {
+      color: '#003479',
+    },
+  },
+  StyleGuide: {
+    '@global html': {
+      boxSizing: 'border-box',
+      margin: 0,
+      padding: 0,
+    },
+    '@global body': {
+      fontFamily: '"Source Sans Pro", sans-serif',
+      margin: 0,
+      padding: 0,
+    },
+    '@global *, *:before, *:after': {
+      boxSizing: 'inherit',
+    },
+  },
+  ReactComponent: {
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    header: {
+      order: '0',
+      marginBottom: '0',
+    },
+    docs: {
+      order: '1',
+      marginBottom: '24px',
+    },
+    tabs: {
+      order: '3',
+    },
+  },
+  Pathline: {
+    pathline: {
+      display: 'none',
+    },
+  },
+  Examples: {
+    root: {
+      order: '2',
+    },
+  },
+  Type: {
+    type: {
+      position: 'relative',
+      display: 'block',
+      overflowY: 'hidden',
+      maxHeight: '4.5em',
+      '&:after': {
+        display: 'block',
+        content: '""',
+        position: 'absolute',
+        top: '0',
+        height: '100%',
+        width: '100%',
+        background:
+          'linear-gradient(180deg, rgba(255, 255, 255, 0) 0, rgba(255, 255, 255, 0) 3em, rgba(255, 255, 255, 1) 4.5em)',
+      },
+    },
+  },
+};
--- styleguide.config.js
+++ styleguide.config.js
@@ -9,7 +9,7 @@ module.exports = {
   assetsDir: path.join(__dirname, '.styleguidist/assets'),
   resolver: require('react-docgen').resolver.findAllComponentDefinitions,
   propsParser: require('react-docgen-typescript').withDefaultConfig({
-    propFilter: { skipPropsWithoutDoc: false },
+    propFilter: { skipPropsWithoutDoc: true },
   }).parse,
   exampleMode: 'expand',
   usageMode: 'expand',
@@ -31,53 +31,5 @@ module.exports = {
       base: '"Source Sans Pro", sans-serif',
     },
   },
-  styles: {
-    Logo: {
-      logo: {
-        color: '#003479',
-      },
-    },
-    StyleGuide: {
-      '@global html': {
-        boxSizing: 'border-box',
-        margin: 0,
-        padding: 0,
-      },
-      '@global body': {
-        fontFamily: '"Source Sans Pro", sans-serif',
-        margin: 0,
-        padding: 0,
-      },
-      '@global *, *:before, *:after': {
-        boxSizing: 'inherit',
-      },
-    },
-    ReactComponent: {
-      root: {
-        display: 'flex',
-        flexDirection: 'column',
-      },
-      header: {
-        order: '0',
-        marginBottom: '0',
-      },
-      docs: {
-        order: '1',
-        marginBottom: '24px',
-      },
-      tabs: {
-        order: '3',
-      },
-    },
-    Pathline: {
-      pathline: {
-        display: 'none',
-      },
-    },
-    Examples: {
-      root: {
-        order: '2',
-      },
-    },
-  },
+  styles: require('./.styleguidist/styleguidist.styles.js'),
 };
