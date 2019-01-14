f6d90a89c163dfe143e57ce015ce5aa885c5c67c
Joonas Kallunki <jkallunki@gmail.com>
2019-01-14 09:34:22 +0200

    Style styleguidist examples with CSS Grid

--- .styleguidist/styleguidist.styles.js
+++ .styleguidist/styleguidist.styles.js
@@ -1,3 +1,10 @@
+const grid440px24px = {
+  display: 'grid',
+  rowGap: '24px',
+  maxWidth: '440px',
+  justifyItems: 'start',
+};
+
 module.exports = {
   Logo: {
     logo: {
@@ -61,6 +68,18 @@ module.exports = {
         width: '100%',
         background:
           'linear-gradient(180deg, rgba(255, 255, 255, 0) 0, rgba(255, 255, 255, 0) 3em, rgba(255, 255, 255, 1) 4.5em)',
+      },
+    },
+  },
+  Playground: {
+    preview: {
+      '& > div, & > div:not(.no-grid)': {
+        ...grid440px24px,
+        '& [example="negative"]': {
+          ...grid440px24px,
+          padding: '24px',
+          backgroundColor: '#2A6EBB',
+        },
       },
     },
   },
