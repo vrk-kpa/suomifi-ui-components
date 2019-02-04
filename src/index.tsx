f0073d59e82e9fb055bbe9d67d70f428d99bd6fe
Joonas Kallunki <jkallunki@gmail.com>
2019-02-04 10:14:11 +0200

    Export icon-component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 import './core/theme/fontFaces.css';
-export { Button, ButtonProps } from './core/Button';
 export { suomifiTheme, ThemeComponent } from './core/theme';
+export { Button, ButtonProps } from './core/Button';
+export { Icon, IconProps } from './core/Icon';
