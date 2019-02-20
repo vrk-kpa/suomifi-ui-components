e165fbfab53c0bb918cbbc5e73e496be34680f32
Joonas Kallunki <jkallunki@gmail.com>
2019-02-20 14:25:07 +0200

    Install @reach/menu-button for used in Dropdown

--- .styleguidist/styleguidist.require.js
+++ .styleguidist/styleguidist.require.js
@@ -1,5 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var ReachMenuButton = require('@reach/menu-button');
+var { MenuItem: DropdownItem, MenuLink: DropdownLink } = ReachMenuButton;
+global.DropdownItem = DropdownItem;
+global.DropdownLink = DropdownLink;
+
 import '../src/core/theme/fontFaces.css';
 
 if (process.env.NODE_ENV !== 'production') {
