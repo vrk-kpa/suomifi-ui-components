d2ad64a03f0cb20f001a579fabd5425c18fcf747
Joonas Kallunki <jkallunki@gmail.com>
2019-03-14 07:52:34 +0200

    Update tests based on Menu component implementation

--- src/core/Menu/Menu.test.tsx
+++ src/core/Menu/Menu.test.tsx
@@ -3,5 +3,5 @@ import { baseStyles } from './Menu.baseStyles';
 
 test('CSS export', () => {
   const css = cssFromBaseStyles(baseStyles);
-  expect(css).toEqual(expect.stringContaining('position'));
+  expect(css).toEqual(expect.stringContaining('data-reach-menu-button'));
 });
