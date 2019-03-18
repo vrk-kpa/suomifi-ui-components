dcd0077d9157c40a6c552e9cc1dbf343e4b97e9a
Joonas Kallunki <jkallunki@gmail.com>
2019-03-18 12:38:38 +0200

    Add HtmlLabel and HtmlInput components

---
+++ src/reset/HtmlInput/HtmlInput.tsx
@@ -0,0 +1,29 @@
+import React, { InputHTMLAttributes } from 'react';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { resets, resetWithSelectors } from '../utils';
+
+export interface HtmlInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  /**
+   * HTML Input type
+   * @default text
+   */
+  type?: string;
+}
+
+const inputResets = css`
+  ${resets.normalize.html}
+  ${resets.normalize.input}
+  ${resetWithSelectors(['::-webkit-input-placeholder'])}
+  ${resets.common}
+  display: inline-block;
+  max-width: 100%;
+`;
+
+const Input = ({ type, ...rest }: HtmlInputProps) => (
+  <input {...rest} type={!!type ? type : 'text'} />
+);
+
+export const HtmlInput = styled(Input)`
+  ${inputResets}
+`;
