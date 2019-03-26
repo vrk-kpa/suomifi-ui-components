838a52b0cc6947f9ac410f8e08ea10b79cec0c14
Joonas Kallunki <jkallunki@gmail.com>
2019-03-26 13:34:14 +0200

    Update tests

--- src/core/Panel/PanelExpansion.test.tsx
+++ src/core/Panel/PanelExpansion.test.tsx
@@ -1,42 +1,39 @@
-// import React from 'react';
-// import { render } from 'react-testing-library';
+import React from 'react';
+import { render } from 'react-testing-library';
 
-// import { PanelExpansion } from './PanelExpansion';
+import { PanelExpansion } from './PanelExpansion';
 import { cssFromBaseStyles } from '../utils';
 import { baseStyles } from './Panel.baseStyles';
 
-// const Title1 = () => (
-//   <span data-testid="panel-expansion-title">Test expansion</span>
-// );
-// const Title2 = () => (
-//   <span data-testid="panel-expansion-title-2">Test expansion two</span>
-// );
+test('calling render with the same component on the same container does not remount', () => {
+  const panelRenderer = render(
+    <PanelExpansion
+      title="Test expansion"
+      titleProps={{ 'data-testid': 'panel-expansion-title' }}
+      className="panel-expansion-test"
+    >
+      Test expansion content
+    </PanelExpansion>,
+  );
+  const { getByTestId, container, rerender } = panelRenderer;
+  expect(container.firstChild).toMatchSnapshot();
+  expect(getByTestId('panel-expansion-title').textContent).toBe(
+    'Test expansion',
+  );
 
-// test('calling render with the same component on the same container does not remount', () => {
-//   const panelRenderer = render(
-//     <PanelExpansion
-//       title={Title1}
-//       titleTag="h3"
-//       className="panel-expansion-test"
-//       data-testid="panel-expansion"
-//     >
-//       Test expansion content
-//     </PanelExpansion>,
-//   );
-//   const { getByTestId, container, rerender } = panelRenderer;
-//   expect(container.firstChild).toMatchSnapshot();
-//   expect(getByTestId('panel-expansion-title').textContent).toBe(
-//     'Test expansion',
-//   );
-
-//   // re-render the same component with different props
-//   rerender(
-//     <PanelExpansion title={Title2}>Test expansion content</PanelExpansion>,
-//   );
-//   expect(getByTestId('panel-expansion-title-2').textContent).toBe(
-//     'Test expansion two',
-//   );
-// });
+  // re-render the same component with different props
+  rerender(
+    <PanelExpansion
+      title="Test expansion two"
+      titleProps={{ 'data-testid': 'panel-expansion-title-2' }}
+    >
+      Test expansion content
+    </PanelExpansion>,
+  );
+  expect(getByTestId('panel-expansion-title-2').textContent).toBe(
+    'Test expansion two',
+  );
+});
 
 test('CSS export', () => {
   const css = cssFromBaseStyles(baseStyles);
