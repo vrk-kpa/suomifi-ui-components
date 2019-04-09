f3d9bd6fc975f0a5d2af4481820fbebeac1b04cd
Joonas Kallunki <jkallunki@gmail.com>
2019-04-09 15:15:38 +0300

    Implement PanelExpansionGroup with suomifi-styles

---
+++ src/core/Panel/PanelExpansionGroup.tsx
@@ -0,0 +1,42 @@
+import React, { Component, ReactElement } from 'react';
+import styled from '@emotion/styled';
+import { withDefaultTheme } from '../theme/utils/defaultTheme';
+import { ThemeComponent } from '../theme';
+import {
+  PanelExpansionGroup as CompPanelExpansionGroup,
+  PanelExpansionGroupProps as CompPanelExpansionGroupProps,
+} from '../../components/Panel/PanelExpansionGroup';
+import { Button, ButtonProps } from '../Button/Button';
+import { baseStyles } from './PanelExpansionGroup.baseStyles';
+
+export interface PanelExpansionGroupProps
+  extends CompPanelExpansionGroupProps,
+    ThemeComponent {}
+
+const StyledPanelExpansionGroup = styled(
+  ({ theme, ...passProps }: PanelExpansionGroupProps) => (
+    <CompPanelExpansionGroup {...passProps} />
+  ),
+)`
+  ${props => baseStyles(props)};
+`;
+
+const OpenAllButton = (children: ReactElement<ButtonProps>) => (
+  <Button.unstyled>{children}</Button.unstyled>
+);
+
+/**
+ * Used for panel style and defined actions
+ */
+export class PanelExpansionGroup extends Component<PanelExpansionGroupProps> {
+  render() {
+    const { openAll, closeAll, ...passProps } = withDefaultTheme(this.props);
+    return (
+      <StyledPanelExpansionGroup
+        {...passProps}
+        OpenAll={OpenAllButton(openAll)}
+        CloseAll={OpenAllButton(closeAll)}
+      />
+    );
+  }
+}
