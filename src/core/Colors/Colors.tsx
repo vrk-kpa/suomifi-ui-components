92d3dd1ebf207c658d9e95a4e6954ca29baafe20
Joonas Kallunki <jkallunki@gmail.com>
2019-04-30 11:21:55 +0300

    Refactor Colors component to have colors-prop, default to theme colors of not given

--- src/core/Colors/Colors.tsx
+++ src/core/Colors/Colors.tsx
@@ -4,6 +4,12 @@ import styled from '@emotion/styled';
 import { ThemeComponent } from '../theme';
 import { baseStyles } from './Colors.baseStyles';
 
+export interface ColorsProps extends ThemeComponent {
+  colors?: {
+    [key: string]: string;
+  };
+}
+
 interface ColorInterface {
   keyName: string;
   color: string;
@@ -11,29 +17,33 @@ interface ColorInterface {
 }
 
 const Color = styled.div`
-  ${(p: ColorInterface) => baseStyles(p.color)};
+  ${({ color }: ColorInterface) => baseStyles(color)};
 `;
 
-export class Colors extends Component<ThemeComponent> {
+const themeColors = (props: ColorsProps) => {
+  const {
+    theme: { colors: themeColors },
+  } = withDefaultTheme(props);
+  return themeColors;
+};
+
+export class Colors extends Component<ColorsProps> {
   render() {
-    const {
-      theme: { colors: themeColors },
-    } = withDefaultTheme(this.props);
-    return Object.entries(themeColors).reduce<JSX.Element[]>(
-      (arr, [key, value]) => {
-        const test = (
-          <Color
-            keyName={key.toString()}
-            color={value.toString()}
-            key={key.toString()}
-          >
-            <div className="fi-color__name">{value.toString()}</div>
-            <div className="fi-color__name">{key.toString()}</div>
-          </Color>
-        );
-        return [...arr, test];
-      },
-      [],
-    );
+    const { colors } = this.props;
+    return Object.entries(!!colors ? colors : themeColors(this.props)).reduce<
+      JSX.Element[]
+    >((arr, [key, value]) => {
+      const test = (
+        <Color
+          keyName={key.toString()}
+          color={value.toString()}
+          key={key.toString()}
+        >
+          <div className="fi-color__name">{value.toString()}</div>
+          <div className="fi-color__name">{key.toString()}</div>
+        </Color>
+      );
+      return [...arr, test];
+    }, []);
   }
 }
