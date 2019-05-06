2915254f0b849f61a635cafcb50142530ccebca1
Joonas Kallunki <jkallunki@gmail.com>
2019-05-06 15:49:41 +0300

    Update exports

--- src/reset/index.ts
+++ src/reset/index.ts
@@ -1,9 +1,10 @@
-export { HtmlButton, HtmlButtonProps } from './HtmlButton/HtmlButton';
-export { HtmlLabel, HtmlLabelProps } from './HtmlLabel/HtmlLabel';
-export { HtmlInput, HtmlInputProps } from './HtmlInput/HtmlInput';
-export { HtmlDiv, HtmlDivProps } from './HtmlDiv/HtmlDiv';
-export { HtmlSpan, HtmlSpanProps } from './HtmlSpan/HtmlSpan';
 export { HtmlA, HtmlAProps } from './HtmlA/HtmlA';
+export { HtmlButton, HtmlButtonProps } from './HtmlButton/HtmlButton';
+export { HtmlDiv, HtmlDivProps } from './HtmlDiv/HtmlDiv';
+export { HtmlH, HtmlHProps } from './HtmlH/HtmlH';
+export { HtmlInput, HtmlInputProps } from './HtmlInput/HtmlInput';
+export { HtmlLabel, HtmlLabelProps } from './HtmlLabel/HtmlLabel';
+export { HtmlLi, HtmlLiProps } from './HtmlLi/HtmlLi';
 export { HtmlNav, HtmlNavProps } from './HtmlNav/HtmlNav';
 export { HtmlOl, HtmlOlProps } from './HtmlOl/HtmlOl';
-export { HtmlLi, HtmlLiProps } from './HtmlLi/HtmlLi';
+export { HtmlSpan, HtmlSpanProps } from './HtmlSpan/HtmlSpan';
