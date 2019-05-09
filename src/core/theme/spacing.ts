2fb51f2dbb78a39e8b6704d6fbb36d95dce2bac5
Joonas Kallunki <jkallunki@gmail.com>
2019-05-09 14:25:31 +0300

    Add spacing tokens

---
+++ src/core/theme/spacing.ts
@@ -0,0 +1,25 @@
+export const spacingTokens = {
+  xxs: '2px',
+  xs: '4px',
+  s: '8px',
+  m: '16px',
+  l: '32px',
+  xl: '64px',
+};
+
+const inset = {
+  squish: {
+    s: `${spacingTokens.xs} ${spacingTokens.s}`,
+    m: `${spacingTokens.s} ${spacingTokens.m}`,
+    l: `${spacingTokens.m} ${spacingTokens.l}`,
+  },
+  stretch: {
+    s: `${spacingTokens.m} ${spacingTokens.s}`,
+    m: `${spacingTokens.l} ${spacingTokens.m}`,
+  },
+};
+
+export const spacing = {
+  ...spacingTokens,
+  inset,
+};
