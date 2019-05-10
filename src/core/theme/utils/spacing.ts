ab2441a1ad95c94bcf0f9f02e6d6be5ab4c309ba
Joonas Kallunki <jkallunki@gmail.com>
2019-05-10 15:21:56 +0300

    Create spacing utils for theme margins and paddings

--- src/core/theme/utils/spacing.ts
+++ src/core/theme/utils/spacing.ts
@@ -1,17 +1,30 @@
-import { spacingTokens } from '../spacing';
+import { spacingTokensProp, spacingTokensKeys } from '../spacing';
+import { suomifiTheme, ThemeProp } from '../';
 
-type SpaceProp = keyof typeof spacingTokens | '0';
+type SpaceProp = spacingTokensProp | '0';
 
-const SpaceVal = (val: SpaceProp) => (val === '0' ? val : spacingTokens[val]);
+const SpaceVal = (theme: ThemeProp) => (val?: SpaceProp) => {
+  if (val === '0') return '0';
+  return !!val ? theme.spacing[val] : '';
+};
 
-const space = (type: 'padding' | 'margin') => (
-  t: SpaceProp,
-  r?: SpaceProp,
-  b?: SpaceProp,
-  l?: SpaceProp,
-) =>
-  `${type}: ${SpaceVal(t)} ${!!r && SpaceVal(r)} ${!!b && SpaceVal(b)} ${!!l &&
-    SpaceVal(l)} ;`;
+const space = (theme: ThemeProp = suomifiTheme) => (
+  type: 'padding' | 'margin',
+) => (t: SpaceProp, r?: SpaceProp, b?: SpaceProp, l?: SpaceProp) =>
+  `${type}: ${SpaceVal(theme)(t)} ${SpaceVal(theme)(r)} ${SpaceVal(theme)(
+    b,
+  )} ${SpaceVal(theme)(l)} ;`;
 
-export const margin = space('margin');
-export const padding = space('padding');
+export const margin = (theme: ThemeProp) => space(theme)('margin');
+export const padding = (theme: ThemeProp) => space(theme)('padding');
+
+export const spacingModifiers = (theme: ThemeProp) => (
+  spacing: 'padding' | 'margin',
+) => (selector: string) =>
+  spacingTokensKeys.reduce(
+    (ret, k) =>
+      `${ret}${selector}-${k}{
+    ${space(theme)(spacing)(k)}
+  } `,
+    '',
+  );
