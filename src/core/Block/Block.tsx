a4d99262089705dc71a6a19e6471ecc571add6e8
Joonas Kallunki <jkallunki@gmail.com>
2019-05-10 15:23:07 +0300

    Create core Block

---
+++ src/core/Block/Block.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
+import { withDefaultTheme } from '../theme/utils';
+import { ThemeComponent } from '../theme';
+import {
+  Block as CompBlock,
+  BlockProps as CompBlockProps,
+} from '../../components/Block/Block';
+import { spacingTokensProp } from '../theme/spacing';
+import { baseStyles } from './Block.baseStyles';
+import classnames from 'classnames';
+
+const baseClassName = 'fi-block';
+
+export interface BlockProps extends CompBlockProps, ThemeComponent {
+  /** Padding from theme */
+  padding?: spacingTokensProp;
+  /** Margin from theme */
+  margin?: spacingTokensProp;
+}
+
+const StyledBlock = styled(
+  ({ theme, className, padding, margin, ...passProps }: BlockProps) => (
+    <CompBlock
+      {...passProps}
+      className={classnames(className, {
+        [`${baseClassName}--padding-${padding}`]: !!padding,
+        [`${baseClassName}--margin-${margin}`]: !!margin,
+        [`${baseClassName}--${passProps.variant}`]: !!passProps.variant,
+      })}
+    />
+  ),
+)`
+  ${props => baseStyles(props)};
+`;
+
+/**
+ * Used displaying Block with correct styles
+ */
+export class Block extends Component<BlockProps> {
+  static section = (props: BlockProps) => (
+    <StyledBlock {...withDefaultTheme(props)} variant="section" />
+  );
+
+  static header = (props: BlockProps) => (
+    <StyledBlock {...withDefaultTheme(props)} variant="header" />
+  );
+
+  static nav = (props: BlockProps) => (
+    <StyledBlock {...withDefaultTheme(props)} variant="nav" />
+  );
+
+  static main = (props: BlockProps) => (
+    <StyledBlock {...withDefaultTheme(props)} variant="main" />
+  );
+
+  static footer = (props: BlockProps) => (
+    <StyledBlock {...withDefaultTheme(props)} variant="footer" />
+  );
+
+  render() {
+    const passProps = withDefaultTheme(this.props);
+    return <StyledBlock {...passProps} />;
+  }
+}
