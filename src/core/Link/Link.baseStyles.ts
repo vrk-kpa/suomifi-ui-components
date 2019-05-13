c729c579ee9eaaa9de47874164aa0ee7a8a53451
Joonas Kallunki <jkallunki@gmail.com>
2019-05-13 07:25:13 +0300

    Add theme focus styles to Link

--- src/core/Link/Link.baseStyles.ts
+++ src/core/Link/Link.baseStyles.ts
@@ -1,12 +1,13 @@
 import { css } from '@emotion/core';
 import { suomifiTheme } from '../theme';
 import { LinkProps } from './Link';
-import { element, fonts } from '../theme/reset';
+import { element, fonts, focus } from '../theme/reset';
 import { allStates } from '../../utils/css/pseudo';
 
 export const baseStyles = ({ theme = suomifiTheme }: LinkProps) => css`
   ${element}
   ${fonts.body}
+  ${focus}
   ${allStates(`color: ${theme.colors.highlightBase};`)};
   color: ${theme.colors.highlightBase};
   text-decoration: none;
