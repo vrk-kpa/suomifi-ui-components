618615eb79ee89efac8a055dcc7a198d0263221b
Joonas Kallunki <jkallunki@gmail.com>
2019-05-23 14:28:48 +0300

    Create SearchInput based on TextInput

--- src/core/Form/Form.tsx
+++ src/core/Form/Form.tsx
@@ -1,2 +1,8 @@
-export { Toggle, ToggleProps } from './Toggle/Toggle';
+export {
+  Toggle,
+  ToggleProps,
+  ToggleInput,
+  ToggleInputProps,
+} from './Toggle/Toggle';
 export { TextInput, TextInputProps } from './TextInput/TextInput';
+export { SearchInput, SearchInputProps } from './SearchInput/SearchInput';
