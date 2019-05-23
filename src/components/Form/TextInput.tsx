eabb7ba0c781d002ffa820213bd93ddd0096df11
Joonas Kallunki <jkallunki@gmail.com>
2019-05-23 08:32:34 +0300

    Create semantic component TextInput

---
+++ src/components/Form/TextInput.tsx
@@ -0,0 +1,98 @@
+import React, { Component, ReactNode, ChangeEvent, FocusEvent } from 'react';
+import { Omit } from '../../utils/typescript';
+import {
+  HtmlLabel,
+  HtmlLabelProps,
+  HtmlInput,
+  HtmlInputProps,
+  HtmlDiv,
+  HtmlDivProps,
+} from '../../reset';
+import { VisuallyHidden } from '../Visually-hidden/Visually-hidden';
+import { Paragraph, ParagraphProps } from '../Paragraph/Paragraph';
+import { logger } from '../../utils/logger';
+import classnames from 'classnames';
+
+const baseClassName = 'fi-text-input';
+const disabledClassName = `${baseClassName}--disabled`;
+const labelClassName = `${baseClassName}-label`;
+const inputClassName = `${baseClassName}-input`;
+
+export interface TextInputLabelProps extends HtmlLabelProps {}
+
+type Label = 'default' | 'screenreader';
+
+export interface TextInputProps extends Omit<HtmlInputProps, 'type'> {
+  /** Custom classname to extend or customize */
+  className?: string;
+  /** Disable input usage */
+  disabled?: boolean;
+  /** Event handler to execute when clicked */
+  onClick?: () => void;
+  /** Pass custom props to label container */
+  labelProps?: TextInputLabelProps;
+  /** Pass custom props to Label text element */
+  labelTextProps?: ParagraphProps;
+  /** To execute on input text change */
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  /** To execute on input text onBlur */
+  onBlur?: (event: FocusEvent<HTMLInputElement>) => void;
+  /** Label */
+  labelText: string;
+  /** Label displaymode
+   * default: show above, screenreader: show as placeholder
+   */
+  labelMode?: Label;
+  /** Input placeholder-text if not using screenreader-labelmode */
+  placeholder?: string;
+  /** Input container div to define custom styling */
+  inputContainerProps?: HtmlDivProps;
+  children?: ReactNode;
+}
+
+export class TextInput extends Component<TextInputProps> {
+  render() {
+    const {
+      className,
+      labelText,
+      labelMode,
+      placeholder,
+      labelProps,
+      labelTextProps,
+      inputContainerProps,
+      children,
+      ...passProps
+    } = this.props;
+
+    const ifScreenreader = labelMode === 'screenreader';
+    if (ifScreenreader && !!placeholder) {
+      logger.warn(
+        'Placeholder not used as label is hidden and using label instead',
+      );
+    }
+
+    return (
+      <HtmlLabel
+        {...labelProps}
+        className={classnames(className, baseClassName, labelClassName, {
+          [disabledClassName]: !!passProps.disabled,
+        })}
+      >
+        {ifScreenreader ? (
+          <VisuallyHidden>{labelText}</VisuallyHidden>
+        ) : (
+          <Paragraph {...labelTextProps}>{labelText}</Paragraph>
+        )}
+        <HtmlDiv {...inputContainerProps}>
+          <HtmlInput
+            {...passProps}
+            className={inputClassName}
+            type="text"
+            placeholder={ifScreenreader ? labelText : placeholder}
+          />
+        </HtmlDiv>
+        {children}
+      </HtmlLabel>
+    );
+  }
+}
