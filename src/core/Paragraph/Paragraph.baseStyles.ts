e2f5ae2b51ef90ec3f1f4183ee96303dc6ab6506
Joonas Kallunki <jkallunki@gmail.com>
2019-05-29 13:40:38 +0300

    Fix: Remove default margin-bottom from Paragraph

--- src/core/Paragraph/Paragraph.baseStyles.ts
+++ src/core/Paragraph/Paragraph.baseStyles.ts
@@ -3,13 +3,15 @@ import { suomifiTheme } from '../theme';
 import { ParagraphProps } from './Paragraph';
 import { element, fonts } from '../theme/reset';
 import { objValue } from '../../utils/typescript';
+import { margin } from '../theme/utils/spacing';
 
 export const baseStyles = ({
   theme = suomifiTheme,
   color,
+  marginBottomSpacing,
 }: ParagraphProps) => css`
   ${element}
   ${fonts.body}
+  ${margin(theme)('0', '0', marginBottomSpacing, '0')};
   color: ${!!color ? objValue(theme.colors, color) : theme.colors.blackBase};
-  margin-bottom: ${theme.spacing.m};
 `;
