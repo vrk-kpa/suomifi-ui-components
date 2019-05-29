0c29e93c6516090edf7e5c42f9e9d4622e5eeb57
Joonas Kallunki <jkallunki@gmail.com>
2019-05-29 13:38:35 +0300

    Fix: as-prop typings was not from styled but from React, worked at runtime not at typescript level

--- src/reset/HtmlA/HtmlA.tsx
+++ src/reset/HtmlA/HtmlA.tsx
@@ -1,12 +1,14 @@
-import React, { ReactNode, HTMLProps } from 'react';
+import React, { ReactNode, HTMLProps, ComponentType } from 'react';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { resets } from '../utils';
 import { allStates } from '../../utils/css/pseudo';
 import { Omit } from '../../utils/typescript';
 
-export interface HtmlAProps extends Omit<HTMLProps<HTMLAnchorElement>, 'ref'> {
+export interface HtmlAProps
+  extends Omit<HTMLProps<HTMLAnchorElement>, 'ref' | 'as'> {
   children: ReactNode;
+  as?: keyof JSX.IntrinsicElements | ComponentType<any>;
 }
 
 const aResets = css`
