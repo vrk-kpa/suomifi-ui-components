04fb2085a918b8a4abe218c898384f0a7adcaa67
Joonas Kallunki <jkallunki@gmail.com>
2019-08-05 09:11:46 +0300

    Add Axe test to TextInput component

--- src/core/Form/TextInput/TextInput.test.tsx
+++ src/core/Form/TextInput/TextInput.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-testing-library';
+import { axeTest } from '../../../utils/test/axe';
+
 import { TextInput } from './TextInput';
 
+const TestTextInput = (
+  <TextInput labelText="Test input" data-testid="textinput" />
+);
+
 test('calling render with the same component on the same container does not remount', () => {
-  const buttonRendered = render(
-    <TextInput labelText="Test input" data-testid="textinput" />,
-  );
+  const buttonRendered = render(TestTextInput);
   const { container } = buttonRendered;
   expect(container.firstChild).toMatchSnapshot();
 });
+
+test('should not have basic accessibility issues', axeTest(TestTextInput));
