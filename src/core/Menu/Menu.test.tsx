1715aa9b9b9407fa1e0f08e35e58f3800260659f
Joonas Kallunki <jkallunki@gmail.com>
2019-08-05 12:23:48 +0300

    Add Axe test to Menu component

--- src/core/Menu/Menu.test.tsx
+++ src/core/Menu/Menu.test.tsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import { axeTest } from '../../utils/test/axe';
 import { cssFromBaseStyles } from '../utils';
+
+import { Menu } from './Menu';
+import { MenuItem, MenuLink } from './MenuItem';
 import { baseStyles } from './Menu.baseStyles';
+
+const doNothing = () => ({});
+
+const TestMenuLanguage = (
+  <Menu.language className="menu-language-test" name="FI">
+    <MenuItem.language onSelect={doNothing} selected={true}>
+      Suomeksi (FI)
+    </MenuItem.language>
+    <MenuLink.language href="/sv">På svenska (SV)</MenuLink.language>
+  </Menu.language>
+);
 
 test('CSS export', () => {
   const css = cssFromBaseStyles(baseStyles);
   expect(css).toEqual(expect.stringContaining('data-reach-menu-button'));
 });
+
+test('should not have basic accessibility issues', axeTest(TestMenuLanguage));
