067095737661c7ba059c50d4939cc9491510f87d
Joonas Kallunki <jkallunki@gmail.com>
2019-08-05 08:19:42 +0300

    Add Jest-Axe

--- src/core/Button/Button.test.tsx
+++ src/core/Button/Button.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import server from 'react-dom/server';
 import { render } from 'react-testing-library';
+import { axe } from 'jest-axe';
 
 import { Button } from './Button';
 import { cssFromBaseStyles } from '../utils';
@@ -14,6 +16,14 @@ test('calling render with the same component on the same container does not remo
   // re-render the same component with different props
   rerender(<Button data-testid="nottub">Test two</Button>);
   expect(getByTestId('nottub').textContent).toBe('Test two');
+});
+
+test('should not have basic accessibility issues', async () => {
+  const html = server.renderToString(
+    <Button data-testid="button">Test</Button>,
+  );
+  const results = await axe(html);
+  expect(results).toHaveNoViolations();
 });
 
 test('CSS export', () => {
