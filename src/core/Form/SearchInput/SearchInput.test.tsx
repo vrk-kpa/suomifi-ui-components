8b57c32b1390668a8a41179fda8ea5f3d8c30b24
Joonas Kallunki <jkallunki@gmail.com>
2019-08-05 09:08:43 +0300

    Add Axe test to SearchInput component

--- src/core/Form/SearchInput/SearchInput.test.tsx
+++ src/core/Form/SearchInput/SearchInput.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-testing-library';
+import { axeTest } from '../../../utils/test/axe';
+
 import { SearchInput } from './SearchInput';
 
+const TestSearchInput = (
+  <SearchInput labelText="Test search input" data-testid="textinput" />
+);
+
 test('calling render with the same component on the same container does not remount', () => {
-  const buttonRendered = render(
-    <SearchInput labelText="Test search input" data-testid="textinput" />,
-  );
+  const buttonRendered = render(TestSearchInput);
   const { container } = buttonRendered;
   expect(container.firstChild).toMatchSnapshot();
 });
+
+test('should not have basic accessibility issues', axeTest(TestSearchInput));
