18b906be3d470de84272b67eb2b9aa008f331bdd
Joonas Kallunki <jkallunki@gmail.com>
2019-08-05 12:30:59 +0300

    Add Axe test to Paragraph component

--- src/core/Paragraph/Paragraph.test.tsx
+++ src/core/Paragraph/Paragraph.test.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-testing-library';
+import { axeTest } from '../../utils/test/axe';
 
 import { Paragraph } from './Paragraph';
 
+const TestParagraph = (
+  <div data-testid="test-paragraph">
+    <Paragraph>Hey this is test</Paragraph>
+  </div>
+);
+
 test('calling render with the same component on the same container does not remount', () => {
-  const ParagraphRendered = render(
-    <div data-testid="test-paragraph">
-      <Paragraph>Hey this is test</Paragraph>
-    </div>,
-  );
+  const ParagraphRendered = render(TestParagraph);
   const { container } = ParagraphRendered;
   expect(container.firstChild).toMatchSnapshot();
 });
+
+test('should not have basic accessibility issues', axeTest(TestParagraph));
