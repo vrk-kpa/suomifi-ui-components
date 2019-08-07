5f0a0b572ac28d0e80a9b80923c535139e4bb7b7
Aappo Ålander <aappoalander@Aappos-MacBook-Pro.local>
2019-08-07 14:05:43 +0300

    Fixed duplicate classname issue and updated test snapshot.

--- src/core/Button/Button.tsx
+++ src/core/Button/Button.tsx
@@ -57,7 +57,7 @@ const StyledButton = styled(
   }: ButtonProps & { right?: boolean }) => (
     <CompButton
       {...passProps}
-      className={classnames(className, baseClassName, {
+      className={classnames(className, {
         [`${baseClassName}--${variant}`]: variant !== 'default',
       })}
     />
