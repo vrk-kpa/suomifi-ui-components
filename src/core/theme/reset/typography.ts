0ba299ca20e3a73ebd3ab1aac18881623d287b6b
Joonas Kallunki <jkallunki@gmail.com>
2019-09-02 12:40:24 +0300

    Rename TS type to uppercase

--- src/core/theme/reset/typography.ts
+++ src/core/theme/reset/typography.ts
@@ -5,9 +5,9 @@ import { typographyTokens, TypographyTokens } from '../typography';
 const camelToSnake = (string: string) =>
   string.replace(/[\w]([A-Z])/g, m => `${m[0]}-${m[1]}`).toLowerCase();
 
-type fontProp = typeof typographyTokens.bodyText;
-type tokenConversionProp = [keyof typeof typographyTokens, fontProp];
-const typographyValueToCss = (value: fontProp) => css`
+type FontProp = typeof typographyTokens.bodyText;
+type tokenConversionProp = [keyof typeof typographyTokens, FontProp];
+const typographyValueToCss = (value: FontProp) => css`
   ${Object.entries(value)
     .map(([key, value]) => `${camelToSnake(key)}: ${value}`)
     .join('')}
