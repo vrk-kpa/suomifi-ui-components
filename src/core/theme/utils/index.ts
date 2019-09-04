3cfca09d1fe365d9539b53f3eb22d21936fe7394
Joonas Kallunki <jkallunki@gmail.com>
2019-09-04 11:40:18 +0300

    Export typograhyUtils

--- src/core/theme/utils/index.ts
+++ src/core/theme/utils/index.ts
@@ -2,3 +2,4 @@ export { withDefaultTheme } from './defaultTheme';
 export { focus } from './focus';
 export { boxshadowOutline } from './outline';
 export { margin, padding, spacingModifiers } from './spacing';
+export { typographyUtils } from './typography';
