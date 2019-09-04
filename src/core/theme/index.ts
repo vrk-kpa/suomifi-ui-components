89f2b35062c6f14ca9f04d653faa7461b2949876
Joonas Kallunki <jkallunki@gmail.com>
2019-09-04 11:41:18 +0300

    Refactor theme so that tokens are first abtraction and suomifiTheme is second abstraction as function

--- src/core/theme/index.ts
+++ src/core/theme/index.ts
@@ -1,28 +1,47 @@
 import { typographyTokens } from './typography';
+import { typographyUtils } from './utils';
 import { colors, shadows, gradients, outlines } from './colors';
 import { spacing } from './spacing';
 import { zindexes } from './zindexes';
 import { transitions } from './transitions';
 import { radius } from './radius';
 
-export type ThemeProp = typeof suomifiTheme;
-export type TypographyProp = keyof typeof suomifiTheme.typography;
-export type ColorProp = keyof typeof suomifiTheme.colors;
-export type SpacingProp = keyof typeof suomifiTheme.spacing;
+export type TokenProp = typeof tokens;
+export type TypographyProp = keyof typeof tokens.typography;
+export type ColorProp = keyof typeof tokens.colors;
+export type SpacingProp = keyof typeof tokens.spacing;
+export type SuomifiThemeProp = typeof suomifiTheme;
 
-export interface ThemeComponent {
+export interface TokensComponent {
   /** Default as suomifiTheme */
-  theme?: ThemeProp;
+  tokens?: TokenProp;
 }
 
-export const suomifiTheme = {
+const tokens = {
   colors,
+  spacing,
+  typography: typographyTokens,
+};
+
+/**
+ *  Theme tokens and tokens as CSS
+ * @param tokens TokensProp, defaults to suomifi-tokens
+ */
+export const suomifiTheme = ({
+  colors,
+  spacing,
+  typography,
+}: TokenProp = tokens) => ({
+  // Internals
   shadows,
   gradients,
   outlines,
-  spacing,
   zindexes,
   transitions,
   radius,
-  typography: typographyTokens,
-};
+  // Tokens
+  colors,
+  spacing,
+  typography: typographyUtils(typography),
+  values: { colors, spacing, typography },
+});
