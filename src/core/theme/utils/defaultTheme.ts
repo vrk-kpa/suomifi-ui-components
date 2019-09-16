7f88b757d5fe788e5093676d44aff1507dc7af36
Joonas Kallunki <jkallunki@gmail.com>
2019-09-16 10:34:12 +0300

    as-prop used by Styled-components is overriding our internal component usage so defaultTheme is changing its' name to asProp for our defined usage

--- src/core/theme/utils/defaultTheme.ts
+++ src/core/theme/utils/defaultTheme.ts
@@ -1,12 +1,21 @@
 import { suomifiTheme, ThemeProp } from '../';
+import { AsProp } from '../../../utils/typescript';
 
+/**
+ * Add default theme to props if not assinged,
+ * also reassign styled as-prop to asProp (it would replace internal component usage with styled-tag)
+ * @param props Component-props
+ */
 export const withDefaultTheme = <
-  T extends { theme: ThemeProp; [k: string]: any }
+  T extends { theme: ThemeProp; as?: AsProp; asProp?: AsProp; [k: string]: any }
 >({
   theme = suomifiTheme,
+  as: asStyled,
+  asProp,
   ...props
 }: Partial<T>): T =>
   ({
     ...props,
     theme,
+    asProp: !!asProp ? asProp : asStyled,
   } as T);
