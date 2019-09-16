d3156828839b81ee6cf1c62edf1acf92d7d6690a
Joonas Kallunki <jkallunki@gmail.com>
2019-09-16 10:09:04 +0300

    Add AsProp type to util exports because Styled Components does not export it from it typings

--- src/utils/typescript.ts
+++ src/utils/typescript.ts
@@ -5,3 +5,6 @@ export type classnamesValue = string | { [key: string]: string | boolean };
 export function objValue<T, K extends keyof T>(obj: T, key: K) {
   return obj[key];
 }
+
+// `as`-prop from styled-components
+export type AsProp = keyof JSX.IntrinsicElements | React.ComponentType<any>;
