1e604e8ad8eb05a9da0d1113bb8889146084a04c
Joonas Kallunki <jkallunki@gmail.com>
2019-09-24 16:11:09 +0300

    Add jsdoc for defaultTheme-function

--- src/core/theme/utils/defaultTheme.ts
+++ src/core/theme/utils/defaultTheme.ts
@@ -1,6 +1,12 @@
 import { suomifiTheme, ThemeProp } from '../';
 import { asPropType } from '../../../utils/typescript';
 
+/**
+ * Do defaults for component props
+ * Add default theme if theme not defined,
+ * Remove as-prop and change it to asProp if defined
+ * @param props Component props
+ */
 export const withDefaultTheme = <
   T extends { theme: ThemeProp; asProp?: asPropType }
 >({
