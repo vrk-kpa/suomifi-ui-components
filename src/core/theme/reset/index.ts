e0b902814b1b993df569b3380ca25718d3c1ae8f
Joonas Kallunki <jkallunki@gmail.com>
2019-10-01 11:28:53 +0300

    Refactor theme reset utils using themeOrTokens

--- src/core/theme/reset/index.ts
+++ src/core/theme/reset/index.ts
@@ -1,62 +1,80 @@
 import { css } from 'styled-components';
-import { TokenProp, suomifiTheme } from '../';
-import { focus as focusUtil } from '../utils';
+import { TypographyProp } from '../';
+import {
+  focus as focusUtil,
+  themeOrTokens,
+  TokensOrThemeProps,
+} from '../utils';
 
-export const focus = (tokens: TokenProp) => focusUtil({ tokens });
+export const focus = (props: TokensOrThemeProps) => focusUtil(props);
 
-export const element = (tokens: TokenProp) => css`
-  color: ${suomifiTheme(tokens).colors.blackBase};
+export const element = (props: TokensOrThemeProps) => css`
+  color: ${themeOrTokens(props).colors.blackBase};
 `;
 
-export const input = (tokens: TokenProp) => css`
-  ${element(tokens)}
-  ${suomifiTheme(tokens).typography.input}
+export const font = (props: TokensOrThemeProps) => (
+  typographyToken: TypographyProp,
+) => css`
+  letter-spacing: 0;
+  text-decoration: none;
+  word-break: break-word;
+  overflow-wrap: break-word;
+  -webkit-font-smoothing: antialiased;
+  ${themeOrTokens(props).typography[typographyToken]}
+`;
+
+export const input = (props: TokensOrThemeProps) => {
+  const theme = themeOrTokens(props);
+  return css`
+    ${element(props)}
+    ${theme.typography.input}
   min-width: 245px;
-  max-width: 100%;
-  padding: ${suomifiTheme(tokens).spacing.s} ${suomifiTheme(tokens).spacing.m};
-  border: 1px solid ${suomifiTheme(tokens).colors.depthBase};
-  border-radius: ${suomifiTheme(tokens).radius.basic};
-  line-height: 1;
-`;
+    max-width: 100%;
+    padding: ${theme.spacing.s} ${theme.spacing.m};
+    border: 1px solid ${theme.colors.depthBase};
+    border-radius: ${theme.radius.basic};
+    line-height: 1;
+  `;
+};
 
-export const inputContainer = (tokens: TokenProp) => css`
+export const inputContainer = (props: TokensOrThemeProps) => css`
   > input:focus {
     /* For IE/Edge */
-    outline-color: ${suomifiTheme(tokens).colors.accentBase};
+    outline-color: ${themeOrTokens(props).colors.accentBase};
     outline-width: 4px;
     outline-offset: 2px;
   }
   &:focus-within {
-    ${focusUtil({ tokens, noPseudo: true })}
+    ${focusUtil({ ...props, noPseudo: true })}
     > input:focus {
       outline: none;
     }
   }
 `;
 
-export const inputButton = (tokens: TokenProp) => css`
-  ${input(tokens)}
-  ${focus(tokens)}
+export const inputButton = (props: TokensOrThemeProps) => css`
+  ${input(props)}
+  ${focus(props)}
 `;
 
-export const button = (tokens: TokenProp) => css`
-  ${element(tokens)}
-  ${suomifiTheme(tokens).typography.inputSemibold}
-  ${focus(tokens)}
+export const button = (props: TokensOrThemeProps) => css`
+  ${element(props)}
+  ${themeOrTokens(props).typography.inputSemibold}
+  ${focus(props)}
   line-height: 1;
 `;
 
-export const nav = (tokens: TokenProp) => css`
-  ${element(tokens)}
+export const nav = (props: TokensOrThemeProps) => css`
+  ${element(props)}
   display: block;
 `;
 
-export const list = (tokens: TokenProp) => css`
-  ${element(tokens)}
+export const list = (props: TokensOrThemeProps) => css`
+  ${element(props)}
   list-style: none;
 `;
 
-export const listItem = (tokens: TokenProp) => css`
-  ${element(tokens)}
+export const listItem = (props: TokensOrThemeProps) => css`
+  ${element(props)}
   list-style: none;
 `;
