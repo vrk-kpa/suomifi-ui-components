2e46d4b68adb3bd6ca15f94af29a9a13e7297f98
Joonas Kallunki <jkallunki@gmail.com>
2019-10-01 11:26:53 +0300

    Export new theme utils

--- src/core/theme/utils/index.ts
+++ src/core/theme/utils/index.ts
@@ -1,4 +1,8 @@
-export { withSuomifiDefaults } from './defaultTokens';
+export {
+  withSuomifiDefaults,
+  themeOrTokens,
+  TokensOrThemeProps,
+} from './defaults';
 export { focus } from './focus';
 export { boxshadowOutline } from './outline';
 export { margin, padding, spacingModifiers } from './spacing';
