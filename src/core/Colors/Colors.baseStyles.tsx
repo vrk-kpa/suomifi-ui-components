a32ff090d0d6db965d4abb7efeb72b3c2c74d05c
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 06:44:57 +0300

    Refactor Colors baseStyles to use withSuomifiTheme

--- src/core/Colors/Colors.baseStyles.tsx
+++ src/core/Colors/Colors.baseStyles.tsx
@@ -1,52 +1,59 @@
 import { css } from 'styled-components';
 import { readableColor } from 'polished';
-import { suomifiTheme } from '../theme';
-import { font } from '../theme/reset';
+import { withSuomifiTheme, SuomifiThemeComponent } from '../theme';
 import { clearfix } from '../../utils/css/utils';
 import { ColorProps } from './Colors';
 
-export const baseStyles = ({ color, theme = suomifiTheme }: ColorProps) => css`
-  ${font(theme).bodyText}
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  float: left;
-  width: 192px;
-  height: 180px;
-  max-width: 100%;
-  padding: ${theme.spacing.s};
-  color: ${readableColor(color)};
-  background-color: ${color};
-  border-bottom: 4px solid ${color};
-  cursor: pointer;
-  z-index: 2;
+export const baseStyles = withSuomifiTheme(
+  ({
+    theme,
+    color: colorProp,
+  }: SuomifiThemeComponent & Partial<ColorProps>) => {
+    const color = !!colorProp ? colorProp : theme.colors.blackBase;
+    return css`
+      ${theme.typography.bodyText}
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      float: left;
+      width: 192px;
+      height: 180px;
+      max-width: 100%;
+      padding: ${theme.spacing.s};
+      color: ${readableColor(color)};
+      background-color: ${color};
+      border-bottom: 4px solid ${color};
+      cursor: pointer;
+      z-index: 2;
 
-  &:hover {
-    border-bottom-color: ${readableColor(color)};
-  }
+      &:hover {
+        border-bottom-color: ${readableColor(color)};
+      }
 
-  .fi-color__name {
-    position: relative;
-    width: 100%;
-    text-align: right;
-    overflow-wrap: break-word;
-    z-index: 3;
-    pointer-events: none;
+      .fi-color__name {
+        position: relative;
+        width: 100%;
+        text-align: right;
+        overflow-wrap: break-word;
+        z-index: 3;
+        pointer-events: none;
 
-    &--hex {
-      ${font(theme).bodyTextSmallScreen}
-      opacity: .4;
-    }
+        &--hex {
+          ${theme.typography.bodyTextSmallScreen}
+          opacity: .4;
+        }
 
-    &--key {
-      ${font(theme).bodySemiBold}
-    }
-  }
+        &--key {
+          ${theme.typography.bodySemiBold}
+        }
+      }
 
-  &:hover .fi-color__name--key {
-    text-decoration: underline;
-  }
-`;
+      &:hover .fi-color__name--key {
+        text-decoration: underline;
+      }
+    `;
+  },
+);
 
 export const containerStyles = css`
   ${clearfix}
