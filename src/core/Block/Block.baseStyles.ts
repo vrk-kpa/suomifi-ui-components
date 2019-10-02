04a19552586441c58162a2f98daca3480fab272f
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 06:46:39 +0300

    Refactor Block baseStyles to use withSuomifiTheme

--- src/core/Block/Block.baseStyles.ts
+++ src/core/Block/Block.baseStyles.ts
@@ -1,12 +1,14 @@
 import { css } from 'styled-components';
-import { suomifiTheme } from '../theme';
 import { BlockProps } from './Block';
-import { element, fonts } from '../theme/reset';
+import { withSuomifiTheme, SuomifiThemeComponent } from '../theme';
+import { element, font } from '../theme/reset';
 import { spacingModifiers } from '../theme/utils';
 
-export const baseStyles = ({ theme = suomifiTheme }: BlockProps) => css`
-  ${element(theme)}
-  ${fonts(theme).body}
-  ${spacingModifiers(theme)('margin')('&.fi-block--margin')}
-  ${spacingModifiers(theme)('padding')('&.fi-block--padding')}
-`;
+export const baseStyles = withSuomifiTheme(
+  ({ theme, tokens }: BlockProps & SuomifiThemeComponent) => css`
+  ${element({ theme })}
+  ${font({ theme })('bodyText')}
+  ${spacingModifiers(tokens)('margin')('&.fi-block--margin')}
+  ${spacingModifiers(tokens)('padding')('&.fi-block--padding')}
+`,
+);
