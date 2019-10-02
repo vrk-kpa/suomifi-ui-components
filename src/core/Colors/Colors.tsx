1c95da4e67bcaa810bc96261175a3bc60a4b2791
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 06:17:42 +0300

    Refactor Colors to use withSuomifiDefaults

--- src/core/Colors/Colors.tsx
+++ src/core/Colors/Colors.tsx
@@ -1,25 +1,25 @@
 import React, { Component, ReactNode } from 'react';
-import { withDefaultTheme } from '../theme/utils';
+import { withSuomifiDefaults } from '../theme/utils';
 import { hslaToHex } from '../../utils/css/colors';
 import { default as styled } from 'styled-components';
-import { ThemeComponent } from '../theme';
+import { TokensComponent, SuomifiThemeComponent } from '../theme';
 import { baseStyles, containerStyles } from './Colors.baseStyles';
 import clipboardCopy from 'clipboard-copy';
 
-export interface ColorsProps extends ThemeComponent {
+export interface ColorsProps extends TokensComponent {
   colors?: {
     [key: string]: string;
   };
 }
 
-export interface ColorProps extends ThemeComponent {
+export interface ColorProps extends TokensComponent {
   keyName: string;
   color: string;
   children?: ReactNode;
 }
 
 const Color = styled.div`
-  ${(props: ColorProps) => baseStyles(props)};
+  ${(props: ColorProps & SuomifiThemeComponent) => baseStyles(props)};
 `;
 
 const ColorsContainer = styled.div`
@@ -31,35 +31,34 @@ const copyKey = (key: string) => () => clipboardCopy(key);
 export class Colors extends Component<ColorsProps> {
   render() {
     const { colors } = this.props;
-    const { theme } = withDefaultTheme(this.props);
+    const props = withSuomifiDefaults(this.props);
     return (
       <ColorsContainer>
-        {Object.entries(!!colors ? colors : theme.colors).reduce<JSX.Element[]>(
-          (arr, [key, value]) => {
-            const hslaAsHex = hslaToHex(value.toString());
-            const item = (
-              <Color
-                keyName={key.toString()}
-                color={value.toString()}
-                key={key.toString()}
-                theme={theme}
-                onClick={copyKey(key.toString())}
-              >
-                <div className="fi-color__name">{value.toString()}</div>
-                {!!hslaAsHex && (
-                  <div className="fi-color__name fi-color__name--hex">
-                    {hslaAsHex}
-                  </div>
-                )}
-                <div className="fi-color__name fi-color__name--key">
-                  {key.toString()}
+        {Object.entries(!!colors ? colors : props.tokens.colors).reduce<
+          JSX.Element[]
+        >((arr, [key, value]) => {
+          const hslaAsHex = hslaToHex(value.toString());
+          const item = (
+            <Color
+              keyName={key.toString()}
+              color={value.toString()}
+              key={key.toString()}
+              onClick={copyKey(key.toString())}
+              {...props}
+            >
+              <div className="fi-color__name">{value.toString()}</div>
+              {!!hslaAsHex && (
+                <div className="fi-color__name fi-color__name--hex">
+                  {hslaAsHex}
                 </div>
-              </Color>
-            );
-            return [...arr, item];
-          },
-          [],
-        )}
+              )}
+              <div className="fi-color__name fi-color__name--key">
+                {key.toString()}
+              </div>
+            </Color>
+          );
+          return [...arr, item];
+        }, [])}
       </ColorsContainer>
     );
   }
