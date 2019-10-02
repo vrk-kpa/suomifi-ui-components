9df0811d918bb0c47a33106bf0356bd2d141ea7d
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 07:05:46 +0300

    Use font reset at Panel baseStyles

--- src/core/Panel/Panel.baseStyles.tsx
+++ src/core/Panel/Panel.baseStyles.tsx
@@ -1,11 +1,11 @@
 import { css } from 'styled-components';
 import { withSuomifiTheme, SuomifiThemeComponent } from '../theme';
-import { element } from '../theme/reset';
+import { element, font } from '../theme/reset';
 
 export const baseStyles = withSuomifiTheme(
   ({ theme }: SuomifiThemeComponent) => css`
   ${element({ theme })}
-  ${theme.typography.bodyText}
+  ${font({ theme })('bodyText')}
   padding: ${theme.spacing.m};
   background-color: ${theme.colors.whiteBase};
 `,
