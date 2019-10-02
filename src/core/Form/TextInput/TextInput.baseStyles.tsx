71f1a925a54671d9837c2195613265de004fea4a
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 06:36:45 +0300

    Refactor TextInput baseStyles to use withSuomifiTheme

--- src/core/Form/TextInput/TextInput.baseStyles.tsx
+++ src/core/Form/TextInput/TextInput.baseStyles.tsx
@@ -1,19 +1,19 @@
 import { css } from 'styled-components';
-import { suomifiTheme } from '../../theme';
-import { TextInputProps } from './TextInput';
+import { withSuomifiTheme, SuomifiThemeComponent } from '../../theme';
 import { input, inputContainer } from '../../theme/reset';
 
-export const baseStyles = ({ theme = suomifiTheme }: TextInputProps) => css`
+export const baseStyles = withSuomifiTheme(
+  ({ theme }: SuomifiThemeComponent) => css`
   & .fi-text-input_label-p {
     margin-bottom: ${theme.spacing.m};
   }
 
   & .fi-text-input_container {
-    ${inputContainer(theme)}
+    ${inputContainer({ theme })}
   }
 
   & .fi-text-input_input {
-    ${input(theme)}
+    ${input({ theme })}
     background-color: ${theme.colors.whiteBase};
   }
 
@@ -29,4 +29,5 @@ export const baseStyles = ({ theme = suomifiTheme }: TextInputProps) => css`
       border-color: ${theme.colors.successBase};
     }
   }
-`;
+`,
+);
