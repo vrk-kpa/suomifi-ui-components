abaf29e3e9b8d3cde8348f587134426f37d92d70
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 06:34:27 +0300

    Refactor Paragraph baseStyles to use withSuomifiTheme

--- src/core/Paragraph/Paragraph.baseStyles.ts
+++ src/core/Paragraph/Paragraph.baseStyles.ts
@@ -1,17 +1,20 @@
 import { css } from 'styled-components';
-import { suomifiTheme } from '../theme';
+import { withSuomifiTheme, SuomifiThemeComponent } from '../theme';
 import { ParagraphProps } from './Paragraph';
-import { element, fonts } from '../theme/reset';
+import { element } from '../theme/reset';
 import { objValue } from '../../utils/typescript';
 import { margin } from '../theme/utils/spacing';
 
-export const baseStyles = ({
-  theme = suomifiTheme,
-  color,
-  marginBottomSpacing = '0',
-}: ParagraphProps) => css`
-  ${element(theme)}
-  ${fonts(theme).body}
-  ${margin(theme)('0', '0', marginBottomSpacing, '0')};
+export const baseStyles = withSuomifiTheme(
+  ({
+    theme,
+    tokens,
+    color,
+    marginBottomSpacing = '0',
+  }: ParagraphProps & SuomifiThemeComponent) => css`
+  ${element({ theme })}
+  ${theme.typography.bodyText}
+  ${margin(tokens)('0', '0', marginBottomSpacing, '0')};
   color: ${!!color ? objValue(theme.colors, color) : theme.colors.blackBase};
-`;
+`,
+);
