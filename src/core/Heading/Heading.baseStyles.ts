e6ddb166e9e23f764e4882fa4f29e3be11f5c707
Joonas Kallunki <jkallunki@gmail.com>
2019-10-02 06:36:07 +0300

    Refactor Heading baseStyles to use withSuomifiTheme

--- src/core/Heading/Heading.baseStyles.ts
+++ src/core/Heading/Heading.baseStyles.ts
@@ -1,64 +1,63 @@
 import { css } from 'styled-components';
-import { suomifiTheme } from '../theme';
+import { withSuomifiTheme, SuomifiThemeComponent } from '../theme';
 import { HeadingProps } from './Heading';
-import { element, fonts } from '../theme/reset';
+import { element, font } from '../theme/reset';
 import { objValue } from '../../utils/typescript';
 
-export const baseStyles = ({
-  theme = suomifiTheme,
-  color,
-}: HeadingProps) => css`
-  ${element(theme)}
-  ${fonts(theme).body}
+export const baseStyles = withSuomifiTheme(
+  ({ theme, color }: SuomifiThemeComponent & Partial<HeadingProps>) => css`
+  ${element({ theme })}
+  ${font({ theme })('bodyText')}
   color: ${!!color ? objValue(theme.colors, color) : theme.colors.blackBase};
 
   &.fi-heading {
     &--h1hero {
-      ${fonts(theme).h1hero}
+      ${font({ theme })('heading1Hero')}
     }
     &--h1 {
-      ${fonts(theme).h1}
+      ${font({ theme })('heading1')}
     }
     &--h2 {
-      ${fonts(theme).h2}
+      ${font({ theme })('heading2')}
     }
     &--h3 {
-      ${fonts(theme).h3}
+      ${font({ theme })('heading3')}
     }
     &--h4 {
-      ${fonts(theme).h4}
+      ${font({ theme })('heading4')}
     }
     &--h5 {
-      ${fonts(theme).h5}
+      ${font({ theme })('heading5')}
     }
     &--h6 {
-      ${fonts(theme).h5}
+      ${font({ theme })('heading5')}
     }
     &--small-screen {
-      ${fonts(theme).smRes.h1}
+      ${font({ theme })('heading1SmallScreen')}
       &.fi-heading {
         &--h1hero {
-          ${fonts(theme).smRes.h1hero}
+          ${font({ theme })('heading1HeroSmallScreen')}
         }
         &--h1 {
-          ${fonts(theme).smRes.h1}
+          ${font({ theme })('heading1SmallScreen')}
         }
         &--h2 {
-          ${fonts(theme).smRes.h2}
+          ${font({ theme })('heading2SmallScreen')}
         }
         &--h3 {
-          ${fonts(theme).smRes.h3}
+          ${font({ theme })('heading3SmallScreen')}
         }
         &--h4 {
-          ${fonts(theme).smRes.h4}
+          ${font({ theme })('heading4SmallScreen')}
         }
         &--h5 {
-          ${fonts(theme).smRes.h5}
+          ${font({ theme })('heading5SmallScreen')}
         }
         &--h6 {
-          ${fonts(theme).smRes.h5}
+          ${font({ theme })('heading5SmallScreen')}
         }
       }
     }
   }
-`;
+`,
+);
