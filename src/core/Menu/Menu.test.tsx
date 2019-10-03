2d8ee7b61190879b6822b6bd9dcd2b4512374ff9
Joonas Kallunki <jkallunki@gmail.com>
2019-10-03 09:02:38 +0300

    Remove commented out tests from Menu
    Currently those tests are not supported with 3rd party lib @reach/menu-button

--- src/core/Menu/Menu.test.tsx
+++ src/core/Menu/Menu.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { axeTest } from '../../utils/test/axe';
-// import { cssFromBaseStyles } from '../utils';
 
 import { Menu } from './Menu';
 import { MenuItem, MenuLink } from './MenuItem';
-// import { baseStyles } from './Menu.baseStyles';
 
 const doNothing = () => ({});
 
@@ -16,10 +14,5 @@ const TestMenuLanguage = (
     <MenuLink.language href="/sv">På svenska (SV)</MenuLink.language>
   </Menu.language>
 );
-
-// test('CSS export', () => {
-//   const css = cssFromBaseStyles(baseStyles);
-//   expect(css).toEqual(expect.stringContaining('data-reach-menu-button'));
-// });
 
 test('should not have basic accessibility issues', axeTest(TestMenuLanguage));
