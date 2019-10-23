cdaec9025a3da71c8895abe5ac8e7578d43d624e
Joonas Kallunki <jkallunki@gmail.com>
2019-10-23 13:32:35 +0300

    Implement suomifi-design-tokens to typography utils and create fontSize util

--- src/core/theme/utils/typography.ts
+++ src/core/theme/utils/typography.ts
@@ -1,20 +1,22 @@
 import {
   cssObjectsToCss,
   FlattenSimpleInterpolation,
+  cssValueToString,
 } from '../../../utils/css';
-import { typographyTokens, TypographyTokens } from '../typography';
+import { tokens, TypograhpyDesingTokens } from 'suomifi-design-tokens';
+import { SuomifiTheme } from '../';
 
 export type TypographyTokensAsCss = {
-  [key in keyof typeof typographyTokens]: string
+  [key in keyof typeof tokens.typography]: string
 };
 
 type TypographyTokensAsCssProp = {
-  [key in keyof TypographyTokens]: FlattenSimpleInterpolation
+  [key in keyof TypograhpyDesingTokens]: FlattenSimpleInterpolation
 };
 export interface TypographyUtil extends TypographyTokensAsCssProp {}
 export class TypographyUtil {
   static instance: TypographyUtil;
-  constructor(tokens: TypographyTokens) {
+  constructor(tokens: TypograhpyDesingTokens) {
     // If instance not created, does not take on account if tokens is different!
     if (!TypographyUtil.instance) {
       // Assing typographyTokens as CSS FlattenSimpleInterpolation to this object
@@ -30,8 +32,18 @@ export class TypographyUtil {
  * @param typography Typography-tokens
  * @return typographyWithUtils Typography-tokens and typography-tokens as CSS strings
  */
-export const typographyUtils = (typography: TypographyTokens) => {
+export const typographyUtils = (typography: TypograhpyDesingTokens) => {
   const instance = new TypographyUtil(typography);
   Object.freeze(instance);
   return instance;
+};
+
+type ValueTypographyTokenProp = keyof typeof tokens.typography;
+export const fontSize = (props: {
+  theme: SuomifiTheme;
+  [key: string]: any | any[];
+}) => (typographyToken: ValueTypographyTokenProp) => {
+  return cssValueToString(
+    props.theme.values.typography[typographyToken].fontSize,
+  );
 };
