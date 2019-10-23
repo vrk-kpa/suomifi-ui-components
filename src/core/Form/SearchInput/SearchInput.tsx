a9bedc4e30f713d28a5533cc6d02af6126b19152
LJKaski <joonas.kaski@gofore.com>
2019-10-23 14:04:35 +0300

    Add conditional placeholder implementation from TextInput to SearchInput

--- src/core/Form/SearchInput/SearchInput.tsx
+++ src/core/Form/SearchInput/SearchInput.tsx
@@ -7,13 +7,22 @@ import { TextInput, TextInputProps } from '../TextInput/TextInput';
 import { Icon } from '../../Icon/Icon';
 import classnames from 'classnames';
 import { Omit } from '../../../utils/typescript';
+import { logger } from '../../../utils/logger';
 
 const baseClassName = 'fi-search-input';
 const inputContainerBaseClassName = `${baseClassName}_input-container`;
 const inputBaseClassName = `${baseClassName}_input`;
 const iconBaseClassName = `${baseClassName}_icon`;
+type Label = 'default' | 'screenreader';
 
-export interface SearchInputProps extends Omit<TextInputProps, 'variant'> {}
+export interface SearchInputProps extends Omit<TextInputProps, 'variant'> {
+  /** Input placeholder-text if not using screenreader-labelmode */
+  placeholder?: string;
+  /** Label displaymode
+   * default: show above, screenreader: show as placeholder
+   */
+  labelMode?: Label;
+}
 
 const StyledTextInput = styled(
   ({
@@ -37,12 +46,20 @@ const StyledTextInput = styled(
  * <i class="semantics" />
  * Use for user inputting search text
  */
-export class SearchInput extends Component<TextInputProps> {
+export class SearchInput extends Component<SearchInputProps> {
   render() {
+    const { labelMode, labelText, placeholder } = this.props;
+
+    const ifScreenreader = labelMode === 'screenreader';
+    if (ifScreenreader && placeholder) {
+      logger.warn(
+        'Placeholder not used as label is hidden and using label instead',
+      );
+    }
     return (
       <StyledTextInput
-        labelMode="screenreader"
         {...withSuomifiDefaultProps(this.props)}
+        placeholder={ifScreenreader ? labelText : placeholder}
       >
         <Icon icon="search" className={iconBaseClassName} />
       </StyledTextInput>
