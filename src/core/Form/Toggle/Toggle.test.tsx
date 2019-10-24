4e1fe704e88a71791659953be92317e86f359574
Sami Korpela <ketsappi@users.noreply.github.com>
2019-10-24 15:37:22 +0300

    Test updated to match the 'withInput' attribute; ToggleInput
    Did not yet change the test name to keep same snapshot for this commit

--- src/core/Form/Toggle/Toggle.test.tsx
+++ src/core/Form/Toggle/Toggle.test.tsx
@@ -6,25 +6,33 @@ import { Toggle } from './Toggle';
 
 const doNothing = () => ({});
 
-const TestToggle = (
-  <Toggle onClick={doNothing} data-testid="toggle" id="test-toggle">
+const TestToggleWithInput = (
+  <Toggle
+    onClick={doNothing}
+    data-testid="toggle"
+    id="test-toggle"
+    withInput={true}
+  >
     Test
   </Toggle>
 );
 
 test('calling render with the same component on the same container does not remount', () => {
-  const buttonRendered = render(TestToggle);
+  const buttonRendered = render(TestToggleWithInput);
   const { getByTestId, container, rerender } = buttonRendered;
   expect(container.firstChild).toMatchSnapshot();
   expect(getByTestId('toggle').textContent).toBe('Test');
 
   // re-render the same component with different props
   rerender(
-    <Toggle onClick={doNothing} data-testid="elggot">
+    <Toggle onClick={doNothing} data-testid="elggot" withInput={true}>
       Test two
     </Toggle>,
   );
   expect(getByTestId('elggot').textContent).toBe('Test two');
 });
 
-test('should not have basic accessibility issues', axeTest(TestToggle));
+test(
+  'should not have basic accessibility issues',
+  axeTest(TestToggleWithInput),
+);
