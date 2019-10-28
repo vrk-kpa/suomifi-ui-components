5ca3d7fb85deab4bfcc61c594f0f2edf325c2cbd
Joonas Kallunki <jkallunki@gmail.com>
2019-10-28 11:45:28 +0200

    Handle all typography suomifi-design-tokens imports at one file

--- src/core/theme/typography.ts
+++ src/core/theme/typography.ts
@@ -2,6 +2,8 @@ import { tokens } from 'suomifi-design-tokens';
 export type TypographyProp = keyof typeof tokens.typography;
 import { cssObjectToCss } from '../../utils/css';
 
+export { TypograhpyDesingTokens } from 'suomifi-design-tokens';
+
 export const typography = tokens.typography;
 
 export const internalTypographyTokens = {
