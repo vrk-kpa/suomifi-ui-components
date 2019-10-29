fbb0a2a901e8af86c61b3f5196df7ab3f4210c12
Joonas Kallunki <jkallunki@gmail.com>
2019-10-29 09:53:49 +0200

    Fix shadows and gradients using tokens format

--- src/core/theme/colors.ts
+++ src/core/theme/colors.ts
@@ -6,15 +6,12 @@ import { zindexes } from './zindexes';
 
 export { ColorDesingTokens } from 'suomifi-design-tokens';
 
-// TODO replace with above one
-export type IColors = typeof colors;
-
 export const { colors } = tokens;
 
 export type IShadows = typeof shadows;
 
 export const shadows = {
-  invertTextShadow: `0 1px 1px ${colors.brandBase}`,
+  invertTextShadow: `0 1px 1px ${colors.brandBase.hsl}`,
   menuShadow: `0 2px 3px 0 ${alphaHex(0.2)(colors.blackBase.hsl)}`,
   panelShadow: `0 1px 2px 0 ${alphaHex(0.14)(
     colors.blackBase.hsl,
@@ -24,26 +21,24 @@ export const shadows = {
 export type IGradients = typeof gradients;
 
 export const gradients = {
-  highlightBase: `linear-gradient(0deg, ${colors.highlightBase} 0%, ${lighten(
-    0.1,
-    colors.highlightBase.hsl,
-  )} 100%)`,
+  highlightBase: `linear-gradient(0deg, ${
+    colors.highlightBase.hsl
+  } 0%, ${lighten(0.1, colors.highlightBase.hsl)} 100%)`,
   highlightLight4: `linear-gradient(0deg, ${
-    colors.highlightLight4
+    colors.highlightLight4.hsl
   } 0%, ${lighten(0.1, colors.highlightLight4.hsl)} 100%)`,
-  highlightDark9: `linear-gradient(0deg, ${colors.highlightDark9} 0%, ${lighten(
-    0.1,
-    colors.highlightDark9.hsl,
-  )} 100%)`,
-  depthBase: `linear-gradient(0deg, ${colors.depthBase} 0%, ${lighten(
+  highlightDark9: `linear-gradient(0deg, ${
+    colors.highlightDark9.hsl
+  } 0%, ${lighten(0.1, colors.highlightDark9.hsl)} 100%)`,
+  depthBase: `linear-gradient(0deg, ${colors.depthBase.hsl} 0%, ${lighten(
     0.1,
     colors.depthBase.hsl,
   )} 100%)`,
   whiteBaseNegative: `linear-gradient(-180deg, ${alphaHex(0.1)(
     colors.whiteBase.hsl,
   )} 0%, ${alphaHex(0)(colors.whiteBase.hsl)} 100%)`,
-  depthLight26: `linear-gradient(0deg, ${colors.depthLight26} 0%, ${
-    colors.whiteBase
+  depthLight26: `linear-gradient(0deg, ${colors.depthLight26.hsl} 0%, ${
+    colors.whiteBase.hsl
   } 100%)`,
   highlightLight45ToHighlightLight50: `linear-gradient(0deg, ${
     colors.highlightLight45
