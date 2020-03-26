a8b5ccfaa832381bd7fc5f659ab3b62b7dd87d99
Aappo Ålander <aappo.alander@gofore.com>
2020-03-26 16:48:57 +0200

    Remove support for inset spacing in block and paragraph components and allow 0 margin and padding for those.

--- src/core/Text/Text.test.tsx
+++ src/core/Text/Text.test.tsx
@@ -8,7 +8,7 @@ import { Text } from './Text';
 const TestTexts = (
   <div data-testid="test-text">
     <Paragraph>Paragraph text</Paragraph>
-    <Paragraph marginBottomSpacing="insetXl">
+    <Paragraph marginBottomSpacing="s">
       <Text.lead>Leading text</Text.lead>
     </Paragraph>
     <Paragraph>
