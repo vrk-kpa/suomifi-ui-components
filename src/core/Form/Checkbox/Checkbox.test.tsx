dfe8f2ce72757a6868ff93c326adc19450feaf83
LJKaski <joonas.kaski@gofore.com>
2020-04-17 08:27:14 +0300

    Add more tests and resulting snapshots

--- src/core/Form/Checkbox/Checkbox.test.tsx
+++ src/core/Form/Checkbox/Checkbox.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { axeTest } from '../../../utils/test/axe';
+import { render } from '@testing-library/react';
 import { Checkbox } from './Checkbox';
 
 const createTestCheckbox = (
@@ -9,6 +10,7 @@ const createTestCheckbox = (
   labelText: string,
   hint?: string,
   statusText?: string,
+  dataTestId?: string,
 ) => (
   <Checkbox
     variant={large ? 'large' : 'small'}
@@ -16,27 +18,61 @@ const createTestCheckbox = (
     status={error ? 'error' : 'default'}
     hintText={hint}
     statusText={statusText}
+    data-testid={dataTestId}
+    id="test"
   >
     {labelText}
   </Checkbox>
 );
 
-const RegularTestCheckbox = createTestCheckbox(false, false, false, 'Regular');
+const RegularTestCheckbox = createTestCheckbox(
+  false,
+  false,
+  false,
+  'Regular',
+  undefined,
+  undefined,
+  'regular_id',
+);
 const LargeTestCheckbox = createTestCheckbox(
   true,
   false,
   false,
-  'Regular',
+  'Large',
   'Take a hint',
+  undefined,
+  'large_id',
 );
 const CheckedLargeTestCheckboxWithError = createTestCheckbox(
   true,
   true,
-  false,
+  true,
   'Large Checked with error',
-  '',
+  undefined,
   'EROR EROR',
+  'largeError_id',
 );
+
+test('Calling render with the same component on the same container does not remount', () => {
+  const toggleInputRendered = render(RegularTestCheckbox);
+  const { getByTestId, container, rerender } = toggleInputRendered;
+  expect(container.firstChild).toMatchSnapshot();
+  expect(getByTestId('regular_id').textContent).toBe('Regular');
+
+  // re-render the same component with different props
+  rerender(
+    createTestCheckbox(
+      false,
+      false,
+      false,
+      'Regular changed',
+      undefined,
+      undefined,
+      'regular_id_changed',
+    ),
+  );
+  expect(getByTestId('regular_id_changed').textContent).toBe('Regular changed');
+});
 
 test(
   'Input should not have basic accessibility issues',
