fd41d13dc08ee069ac88939452ae6b9679ce6a58
Aappo Ålander <aappo.alander@gofore.com>
2020-04-29 14:44:25 +0300

    Refactor for Toggle tests, add more tests to simulate clicks.

--- src/core/Form/Toggle/Toggle.test.tsx
+++ src/core/Form/Toggle/Toggle.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { axeTest } from '../../../utils/test/axe';
 
 import { Toggle } from './Toggle';
@@ -7,51 +7,76 @@ import { Toggle } from './Toggle';
 const doNothing = () => ({});
 
 const CreateTestToggle = (
-  withInput: boolean,
+  variant: 'withInput' | 'default',
   text: string,
   dataTestId: string,
+  onClick: () => {} = doNothing,
 ) => (
   <Toggle
-    onClick={doNothing}
+    onClick={onClick}
     data-testid={dataTestId}
     id="test-toggle"
-    variant={withInput ? 'withInput' : 'default'}
+    variant={variant}
   >
     {text}
   </Toggle>
 );
 
-const TestToggleWithInput = CreateTestToggle(true, 'Test', 'toggle');
-const TestToggleWithButton = CreateTestToggle(false, 'Test', 'toggle');
+describe.each([['withInput'], ['default']])(
+  'Toggle variant %s: ',
+  (variant: 'withInput' | 'default') => {
+    test('calling render with the same component on the same container does not remount', () => {
+      const toggleInputRendered = render(
+        CreateTestToggle(variant, 'Test', 'toggle'),
+      );
+      const { getByTestId, container, rerender } = toggleInputRendered;
+      expect(container.firstChild).toMatchSnapshot();
+      expect(getByTestId('toggle').textContent).toBe('Test');
 
-test('Input: calling render with the same component on the same container does not remount', () => {
-  const toggleInputRendered = render(TestToggleWithInput);
-  const { getByTestId, container, rerender } = toggleInputRendered;
-  expect(container.firstChild).toMatchSnapshot();
-  expect(getByTestId('toggle').textContent).toBe('Test');
+      // re-render the same component with different props
+      rerender(CreateTestToggle(variant, 'Test two', 'elggot'));
+      expect(getByTestId('elggot').textContent).toBe('Test two');
+    });
 
-  // re-render the same component with different props
-  rerender(CreateTestToggle(true, 'Test two', 'elggot'));
-  expect(getByTestId('elggot').textContent).toBe('Test two');
-});
+    test('onClick should activate toggle and deactivate when clicked again', async () => {
+      const mockClickHandler = jest.fn();
+      const { getByTestId, container } = render(
+        CreateTestToggle(variant, 'Test two', 'elggot', mockClickHandler),
+      );
+      const toggle = getByTestId('elggot');
+      fireEvent.click(toggle);
+      expect(mockClickHandler).toHaveBeenCalledTimes(1);
+      const svgClassList = container.querySelector('svg')?.classList;
+      expect(svgClassList).toContain('fi-toggle_icon');
+      expect(svgClassList).toContain('fi-toggle_icon--checked');
+      fireEvent.click(toggle);
+      expect(svgClassList).not.toContain('fi-toggle_icon--checked');
+    });
 
-test('Button: calling render with the same component on the same container does not remount', () => {
-  const toggleButtonRendered = render(TestToggleWithButton);
-  const { getByTestId, container, rerender } = toggleButtonRendered;
-  expect(container.firstChild).toMatchSnapshot();
-  expect(getByTestId('toggle').textContent).toBe('Test');
+    test('onClick should not change toggle state when checked is controlled', async () => {
+      const mockClickHandler = jest.fn();
+      const { getByTestId, container } = render(
+        <Toggle
+          variant={variant}
+          data-testid={'elggot'}
+          checked={false}
+          id="test-toggle"
+          onClick={mockClickHandler}
+        >
+          Controlled checked state
+        </Toggle>,
+      );
+      const toggle = getByTestId('elggot');
+      fireEvent.click(toggle);
+      expect(mockClickHandler).toHaveBeenCalledTimes(1);
+      const svgClassList = container.querySelector('svg')?.classList;
+      expect(svgClassList).toContain('fi-toggle_icon');
+      expect(svgClassList).not.toContain('fi-toggle_icon--checked');
+    });
 
-  // re-render the same component with different props
-  rerender(CreateTestToggle(false, 'Test two', 'elggot'));
-  expect(getByTestId('elggot').textContent).toBe('Test two');
-});
-
-test(
-  'Input: should not have basic accessibility issues',
-  axeTest(TestToggleWithInput),
-);
-
-test(
-  'Button: should not have basic accessibility issues',
-  axeTest(TestToggleWithButton),
+    test(
+      'should not have basic accessibility issues',
+      axeTest(CreateTestToggle(variant, 'Test', 'toggle')),
+    );
+  },
 );
