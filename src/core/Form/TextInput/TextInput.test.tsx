e23927234ec5949ecfc5691415dab9bb64e3c960
LJKaski <joonas.kaski@gofore.com>
2020-06-11 12:51:34 +0300

    Add more tests to cover more of the API

--- src/core/Form/TextInput/TextInput.test.tsx
+++ src/core/Form/TextInput/TextInput.test.tsx
@@ -8,8 +8,41 @@ const TestTextInput = (
   <TextInput labelText="Test input" data-testid="textinput" id="test-id" />
 );
 
+const TestTextInput2 = (
+  <TextInput
+    labelText="Test input"
+    data-testid="textinput1"
+    id="test-id1"
+    labelMode="hidden"
+    visualPlaceholder="Test TextInput"
+  />
+);
+
+const TestTextInput3 = (
+  <TextInput
+    labelText="Test input"
+    data-testid="textinput2"
+    id="test-id2"
+    visualPlaceholder="Test TextInput"
+    statusText="This is a status text"
+    status="error"
+  />
+);
+
 test('calling render with the same component on the same container does not remount', () => {
   const buttonRendered = render(TestTextInput);
+  const { container } = buttonRendered;
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('calling render with the same component on the same container does not remount', () => {
+  const buttonRendered = render(TestTextInput2);
+  const { container } = buttonRendered;
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('calling render with the same component on the same container does not remount', () => {
+  const buttonRendered = render(TestTextInput3);
   const { container } = buttonRendered;
   expect(container.firstChild).toMatchSnapshot();
 });
