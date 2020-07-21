5019450e38cbe57bb5a719286ad0ae6f7d8ce6e2
Sami Korpela <ketsappi@users.noreply.github.com>
2020-07-21 11:02:49 +0300

    Tests added for className, name, value and hintText props of Radiobutton

--- src/core/Form/Radiobutton/Radiobutton.test.tsx
+++ src/core/Form/Radiobutton/Radiobutton.test.tsx
@@ -16,3 +16,41 @@ describe('disabled', () => {
     expect(getByRole('radio')).toHaveAttribute('disabled');
   });
 });
+
+describe('className', () => {
+  it('has the given custom className', () => {
+    const { container } = render(
+      <Radiobutton className="custom-className">Option 1</Radiobutton>,
+    );
+    expect(container.firstChild).toHaveClass('custom-className');
+  });
+});
+
+describe('name', () => {
+  it('has the given "name"-attribute', () => {
+    const { getByRole } = render(
+      <Radiobutton name="group-name">Option 1</Radiobutton>,
+    );
+    expect(getByRole('radio')).toHaveAttribute('name', 'group-name');
+  });
+});
+
+describe('value', () => {
+  it('has the given "value"-attribute', () => {
+    const { getByRole } = render(
+      <Radiobutton value="option-value-1">Option 1</Radiobutton>,
+    );
+    expect(getByRole('radio')).toHaveAttribute('value', 'option-value-1');
+  });
+});
+
+describe('hintText', () => {
+  it('has element for hintText when prop is given', () => {
+    const { getByText } = render(
+      <Radiobutton hintText="Example hint text">Option 1</Radiobutton>,
+    );
+    const hintText = getByText('Example hint text');
+    expect(hintText.textContent).toBe('Example hint text');
+    expect(hintText).toHaveClass('fi-radiobutton_hintText');
+  });
+});
