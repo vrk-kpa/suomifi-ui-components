c3ab62b3d145abdb60fa8851dcc6d1cb3dfc55aa
Sami Korpela <ketsappi@users.noreply.github.com>
2020-07-23 10:40:11 +0300

    Snapshot tests for every prop of the RadiobuttonGroup and default
    Maybe too excessive amount, but we might catch unintended changes little better than without

--- src/core/Form/Radiobutton/RadiobuttonGroup.test.tsx
+++ src/core/Form/Radiobutton/RadiobuttonGroup.test.tsx
@@ -9,9 +9,22 @@ const RadioChildren = [1, 2, 3].map((value) => (
   </Radiobutton>
 ));
 
-describe('className', () => {
-  it('has the given classname', () => {
-    const { container } = render(
+describe('default, with only required props', () => {
+  const DefaultGroup = (
+    <RadiobuttonGroup id="test-id" label="Label" name="name">
+      {RadioChildren}
+    </RadiobuttonGroup>
+  );
+
+  it('should match snapshot', () => {
+    const { container } = render(DefaultGroup);
+    expect(container).toMatchSnapshot();
+  });
+});
+
+describe('props', () => {
+  describe('className', () => {
+    const ClassnameGroup = (
       <RadiobuttonGroup
         id="test-id"
         label="Label"
@@ -19,15 +32,22 @@ describe('className', () => {
         className="custom-className"
       >
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    expect(container.firstChild).toHaveClass('custom-className');
-  });
-});
 
-describe('hintText', () => {
-  it('has hintText element', () => {
-    const { getByText } = render(
+    it('has the given classname', () => {
+      const { container } = render(ClassnameGroup);
+      expect(container.firstChild).toHaveClass('custom-className');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(ClassnameGroup);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('hintText', () => {
+    const HintTextGroup = (
       <RadiobuttonGroup
         id="test-id"
         label="Label"
@@ -35,28 +55,42 @@ describe('hintText', () => {
         hintText="Example hint text"
       >
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    const hintText = getByText('Example hint text');
-    expect(hintText).toHaveClass('fi-radiobuttongroup_hintText');
-  });
-});
 
-describe('label', () => {
-  it('has label element', () => {
-    const { getByText } = render(
+    it('has hintText element', () => {
+      const { getByText } = render(HintTextGroup);
+      const hintText = getByText('Example hint text');
+      expect(hintText).toHaveClass('fi-radiobuttongroup_hintText');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(HintTextGroup);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('label', () => {
+    const LabelGroup = (
       <RadiobuttonGroup id="test-id" label="Label here" name="name">
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    const label = getByText('Label here');
-    expect(label).toHaveClass('fi-radiobuttongroup_label');
-  });
-});
 
-describe('labelMode', () => {
-  it('has visually hidden label element', () => {
-    const { getByText } = render(
+    it('has label element', () => {
+      const { getByText } = render(LabelGroup);
+      const label = getByText('Label here');
+      expect(label).toHaveClass('fi-radiobuttongroup_label');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(LabelGroup);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('labelMode', () => {
+    const LabelModeGroup = (
       <RadiobuttonGroup
         id="test-id"
         label="Label here"
@@ -64,70 +98,107 @@ describe('labelMode', () => {
         labelMode="hidden"
       >
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    const label = getByText('Label here');
-    expect(label).toHaveClass('fi-visually-hidden');
-  });
-});
 
-describe('id', () => {
-  it('has the given id', () => {
-    const { container } = render(
+    it('has visually hidden label element', () => {
+      const { getByText } = render(LabelModeGroup);
+      const label = getByText('Label here');
+      expect(label).toHaveClass('fi-visually-hidden');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(LabelModeGroup);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('id', () => {
+    const IdGroup = (
       <RadiobuttonGroup id="good-id" label="Label" name="name">
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    expect(container.firstChild).toHaveAttribute('id', 'good-id');
-  });
-});
 
-describe('name', () => {
-  it('has children, which all have the given name', () => {
-    const { getAllByRole } = render(
+    it('has the given id', () => {
+      const { container } = render(IdGroup);
+      expect(container.firstChild).toHaveAttribute('id', 'good-id');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(IdGroup);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('name', () => {
+    const NameGroup = (
       <RadiobuttonGroup id="test-id" label="Label" name="nice-name">
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    const radios = getAllByRole('radio');
-    radios.map((radio) => expect(radio).toHaveAttribute('name', 'nice-name'));
-  });
-});
 
-describe('value', () => {
-  it('has the correct radio selected', () => {
-    const { container, rerender } = render(
+    it('has children, which all have the given name', () => {
+      const { getAllByRole } = render(NameGroup);
+      const radios = getAllByRole('radio');
+      radios.map((radio) => expect(radio).toHaveAttribute('name', 'nice-name'));
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(NameGroup);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('value', () => {
+    const ValueGroup = (
       <RadiobuttonGroup id="test-id" label="Label" name="name" value="2">
         {RadioChildren}
-      </RadiobuttonGroup>,
-    );
-    const radiobuttons = container.querySelectorAll('.fi-radiobutton');
-    expect(radiobuttons[0]).not.toHaveClass('fi-radiobutton--checked');
-    expect(radiobuttons[1]).toHaveClass('fi-radiobutton--checked');
-    expect(radiobuttons[2]).not.toHaveClass('fi-radiobutton--checked');
-
-    rerender(
-      <RadiobuttonGroup id="test-id" label="Label" name="name" value="3">
-        {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
 
-    expect(radiobuttons[0]).not.toHaveClass('fi-radiobutton--checked');
-    expect(radiobuttons[1]).not.toHaveClass('fi-radiobutton--checked');
-    expect(radiobuttons[2]).toHaveClass('fi-radiobutton--checked');
+    it('has the correct radio selected', () => {
+      const { container, rerender } = render(ValueGroup);
+      const radiobuttons = container.querySelectorAll('.fi-radiobutton');
+      expect(radiobuttons[0]).not.toHaveClass('fi-radiobutton--checked');
+      expect(radiobuttons[1]).toHaveClass('fi-radiobutton--checked');
+      expect(radiobuttons[2]).not.toHaveClass('fi-radiobutton--checked');
+
+      rerender(
+        <RadiobuttonGroup id="test-id" label="Label" name="name" value="3">
+          {RadioChildren}
+        </RadiobuttonGroup>,
+      );
+
+      expect(radiobuttons[0]).not.toHaveClass('fi-radiobutton--checked');
+      expect(radiobuttons[1]).not.toHaveClass('fi-radiobutton--checked');
+      expect(radiobuttons[2]).toHaveClass('fi-radiobutton--checked');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(ValueGroup);
+      expect(container).toMatchSnapshot();
+    });
   });
-});
 
-describe('defaultValue', () => {
-  it('has the correct radio selected by default', () => {
-    const { container } = render(
+  describe('defaultValue', () => {
+    const DefaultValueGroup = (
       <RadiobuttonGroup id="test-id" label="Label" name="name" defaultValue="3">
         {RadioChildren}
-      </RadiobuttonGroup>,
+      </RadiobuttonGroup>
     );
-    const radiobuttons = container.querySelectorAll('.fi-radiobutton');
-    expect(radiobuttons[0]).not.toHaveClass('fi-radiobutton--checked');
-    expect(radiobuttons[1]).not.toHaveClass('fi-radiobutton--checked');
-    expect(radiobuttons[2]).toHaveClass('fi-radiobutton--checked');
+
+    it('has the correct radio selected by default', () => {
+      const { container } = render(DefaultValueGroup);
+      const radiobuttons = container.querySelectorAll('.fi-radiobutton');
+      expect(radiobuttons[0]).not.toHaveClass('fi-radiobutton--checked');
+      expect(radiobuttons[1]).not.toHaveClass('fi-radiobutton--checked');
+      expect(radiobuttons[2]).toHaveClass('fi-radiobutton--checked');
+    });
+
+    it('should match snapshot', () => {
+      const { container } = render(DefaultValueGroup);
+      expect(container).toMatchSnapshot();
+    });
   });
 });
