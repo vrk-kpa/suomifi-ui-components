673748d88014ed595d1d9fb62e15ab36931d5754
Sami Korpela <ketsappi@users.noreply.github.com>
2020-07-28 13:21:42 +0300

    Fix the problem when using static large variant of the Radiobutton
    Checking if the child has value prop, that basically should be required

--- src/components/Form/RadiobuttonGroup.tsx
+++ src/components/Form/RadiobuttonGroup.tsx
@@ -5,7 +5,6 @@ import { VisuallyHidden } from '../Visually-hidden/Visually-hidden';
 import { idGenerator } from '../../utils/uuid';
 
 import { RadiobuttonProps } from './Radiobutton';
-import { Radiobutton as CoreRadiobutton } from '../../core/Form/Radiobutton/Radiobutton';
 
 const baseClassName = 'fi-radiobuttongroup';
 const radiobuttonGroupClassNames = {
@@ -65,7 +64,7 @@ const RadiobuttonGroupItems = (
   React.Children.map(
     children,
     (child: React.ReactElement<RadiobuttonProps>) => {
-      if (React.isValidElement(child) && child.type === CoreRadiobutton) {
+      if (React.isValidElement(child) && child.props.value) {
         return React.cloneElement(child, {
           radiobuttonGroup: true,
           checked: selectedValue === child.props.value,
