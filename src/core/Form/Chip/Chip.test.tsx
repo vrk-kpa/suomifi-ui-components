15ec07ee0ea5fb5a98fb81f9e210d2e745a80d2f
LJKaski <joonas.kaski@gofore.com>
2020-09-09 13:51:13 +0300

    create test suite for chip component

---
+++ src/core/Form/Chip/Chip.test.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Chip as OrigChip, ChipProps } from './Chip';
+
+const Chip = (props: ChipProps) => {
+  const { children, ...passProps } = props;
+  return <OrigChip {...passProps}>{children || 'Chip content'}</OrigChip>;
+};
+
+describe('disabled', () => {
+  const DisabledChip = <Chip disabled={true} />;
+
+  it('has "--disabled"-class', () => {
+    const { container } = render(DisabledChip);
+    expect(container.firstChild).toHaveClass('fi-chip--disabled');
+  });
+
+  it('will add "disabled"-attribute for the button element', () => {
+    const { getByRole } = render(DisabledChip);
+    expect(getByRole('button')).toHaveAttribute('disabled');
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(DisabledChip);
+    expect(container).toMatchSnapshot();
+  });
+});
+
+describe('children', () => {
+  const chipWithChildren = (
+    <Chip>
+      <div data-testid="test-div">Selection 1</div>
+    </Chip>
+  );
+
+  it('has the given content', () => {
+    const { getByTestId } = render(chipWithChildren);
+    expect(getByTestId('test-div').textContent).toBe('Selection 1');
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(chipWithChildren);
+    expect(container).toMatchSnapshot();
+  });
+});
+
+describe('classnames', () => {
+  const customClassChip = <Chip className="custom-class" />;
+
+  it('contains base classname', () => {
+    const { container } = render(customClassChip);
+    expect(container.firstChild).toHaveClass('fi-chip');
+  });
+
+  it('contains custom classname', () => {
+    const { container } = render(customClassChip);
+    expect(container.firstChild).toHaveClass('custom-class');
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(customClassChip);
+    expect(container).toMatchSnapshot();
+  });
+});
+
+describe('variants', () => {
+  const removableChip = <Chip removable={true} />;
+
+  it('has the elements corresponding the variant', () => {
+    const { container } = render(removableChip);
+    const icon = container.querySelector('#jepjoo');
+    expect(container.contains(icon)).toBeTruthy();
+  });
+});
