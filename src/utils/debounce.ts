87afee0208798fecc2bd24a1aea2aef33b456271
LJKaski <joonas.kaski@gofore.com>
2020-11-25 08:52:45 +0200

    add basic debounce function to utils

---
+++ src/utils/debounce.ts
@@ -0,0 +1,61 @@
+/** Debounce wrapper for functions */
+export const debounce = <F extends (...args: any[]) => any>(
+  func: F,
+  waitFor: number,
+) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  const debounced = (...args: Parameters<F>) => {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced as (...args: Parameters<F>) => ReturnType<F>;
+};
+
+/**
+ * A function that emits a side effect and does not return anything.
+ */
+/*
+export type Procedure = (...args: any[]) => void;
+
+export type Options = {
+  isImmediate: boolean,
+}
+
+export function debounce<F extends Procedure>(
+  func: F,
+  waitMilliseconds = 50,
+  options: Options = {
+    isImmediate: false
+  },
+): (this: ThisParameterType<F>, ...args: Parameters<F>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
+
+  return function(this: ThisParameterType<F>, ...args: Parameters<F>) {
+    const context = this;
+
+    const doLater = function() {
+      timeoutId = undefined;
+      if (!options.isImmediate) {
+        func.apply(context, args);
+      }
+    }
+
+    const shouldCallNow = options.isImmediate && timeoutId === undefined;
+
+    if (timeoutId !== undefined) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(doLater, waitMilliseconds);
+
+    if (shouldCallNow) {
+      func.apply(context, args);
+    }
+  }
+}
+*/
