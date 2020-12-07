dcca11a99b3daa01859e9a53b0b26d8aa7978164
LJKaski <joonas.kaski@gofore.com>
2020-12-07 09:28:13 +0200

    add workingcomponent implementation

--- src/core/utils/Debounce/Debounce.tsx
+++ src/core/utils/Debounce/Debounce.tsx
@@ -1,46 +1,37 @@
-import React from 'react';
-
+import { Component } from 'react';
 export type DebounceProps<T> = T & {
   waitFor: number;
 };
-
-export class Debounce<T extends Object> extends React.PureComponent<
-  DebounceProps<T>,
-  T
-> {
-  timeout: ReturnType<typeof setTimeout> | null = null;
-
-  waitFor: number;
-
-  constructor(props: DebounceProps<T>) {
-    super(props);
-    this.waitFor = props.waitFor;
-    console.log(`hello from constructor. Waitfor value is ${this.waitFor}`);
-  }
+export class Debounce<T extends Object> extends Component<DebounceProps<T>> {
+  state: { timeout: ReturnType<typeof setTimeout> | null; waitFor: number } = {
+    timeout: null,
+    waitFor: 0,
+  };
 
   componentDidUpdate() {
-    // casted to any due to https://github.com/Microsoft/TypeScript/issues/10727
-
-    this.clearTimeout();
-    this.waitFor = this.props.waitFor;
+    if (this.props.waitFor !== this.state.waitFor) {
+      this.setState({ waitFor: this.props.waitFor });
+    }
   }
 
   componentWillUnmount() {
     this.clearTimeout();
   }
 
-  debouncer = (callback: Function, value: any) => {
+  debouncer = (callback: Function, value: T) => {
     this.clearTimeout();
-    this.timeout = setTimeout(callback(value), this.waitFor);
-    console.log(
-      `hello from debouncer func. Waitfor value is ${this.waitFor}, callBack is ${callback} and value is ${value}`,
-    );
+    const { waitFor } = this.state;
+    this.setState({
+      timeout: setTimeout(() => {
+        callback(value);
+      }, waitFor),
+    });
   };
 
   clearTimeout() {
-    if (this.timeout !== null) {
-      clearTimeout(this.timeout);
-      this.timeout = null;
+    if (this.state.timeout !== null) {
+      clearTimeout(this.state.timeout);
+      this.setState({ timeout: null });
     }
   }
 
