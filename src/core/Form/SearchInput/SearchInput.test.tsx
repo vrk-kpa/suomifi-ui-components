d095c8244c26c11e0e1511a8402e47e1aa201e77
LJKaski <joonas.kaski@gofore.com>
2020-12-17 08:36:56 +0200

    clear up naming of a searchinput test

--- src/core/Form/SearchInput/SearchInput.test.tsx
+++ src/core/Form/SearchInput/SearchInput.test.tsx
@@ -221,7 +221,7 @@ describe('props', () => {
       expect(mockOnChange).toBeCalledTimes(1);
       expect(inputElement.value).toBe('new value');
     });
-    it('resolves right when no onChange is given', () => {
+    it('resolves right when no callback is given to onChange', () => {
       const searchInput = (
         <SearchInput
           labelText="Debounced search"
