2bb80195b18c06850856bbccab562a9a6c10bfd0
Sami Korpela <ketsappi@users.noreply.github.com>
2021-01-15 11:41:01 +0200

    Add separate ComboboxCheckboxItem draft to be used

---
+++ src/core/Form/Combobox/ComboboxCheckboxItem.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { default as styled } from 'styled-components';
+import classnames from 'classnames';
+import { HtmlDiv } from '../../../reset';
+import { TokensProp, InternalTokensProp } from '../../theme';
+import { withSuomifiDefaultProps } from '../../theme/utils';
+import { CheckboxProps, Checkbox } from '../Checkbox/Checkbox';
+import { baseStyles } from './ComboboxItem.baseStyles';
+
+const baseClassName = 'fi-combobox-item';
+
+const comboboxItemClassNames = {
+  wrapper: `${baseClassName}_wrapper`,
+};
+
+export interface ComboboxItemProps extends CheckboxProps, TokensProp {
+  /** ComboboxItem container div class name for custom styling. */
+  className?: string;
+}
+
+class BaseComboboxItem extends Component<ComboboxItemProps> {
+  render() {
+    const { className, children, ...passProps } = this.props;
+    return (
+      <HtmlDiv className={classnames(baseClassName, className, {})}>
+        <HtmlDiv className={comboboxItemClassNames.wrapper}>
+          <Checkbox {...passProps}>{children}</Checkbox>
+        </HtmlDiv>
+      </HtmlDiv>
+    );
+  }
+}
+
+const StyledComboboxItem = styled(
+  ({ tokens, ...passProps }: ComboboxItemProps & InternalTokensProp) => (
+    <BaseComboboxItem {...passProps} />
+  ),
+)`
+  ${(props) => baseStyles(props)}
+`;
+
+export class ComboboxItem extends Component<ComboboxItemProps> {
+  render() {
+    return <StyledComboboxItem {...withSuomifiDefaultProps(this.props)} />;
+  }
+}
