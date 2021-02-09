3f76c300b34a48a06e47e5c2a0228318130e2ff8
Sami Korpela <ketsappi@users.noreply.github.com>
2021-02-09 15:21:41 +0200

    Change to use RefObject instead of Element for input ref

--- src/core/Form/FilterInput/FilterInput.tsx
+++ src/core/Form/FilterInput/FilterInput.tsx
@@ -63,7 +63,7 @@ interface InternalFilterInputProps<T>
   onFilter: (filteredItems: Array<T>) => void;
   /** Filtering rule to be used */
   filterFunc: (item: T, query: string) => boolean;
-  forwardRef?: (element: Element | null) => void;
+  forwardRef?: React.RefObject<HTMLInputElement>;
 }
 
 interface InnerRef {
