ad6ea62c1d45f3d1e55e6d03ba62c12f0c819230
Sami Korpela <ketsappi@users.noreply.github.com>
2021-02-09 10:53:16 +0200

    Add icon to indicate if list is open/closed

--- src/core/Form/Combobox/Combobox.baseStyles.tsx
+++ src/core/Form/Combobox/Combobox.baseStyles.tsx
@@ -7,6 +7,22 @@ export const baseStyles = withSuomifiTheme(
     &.fi-combobox {
       ${font({ theme })('bodyText')}
       width: 290px;
+
+      & .fi-filter-input_input-element-container {
+        position: relative;
+
+        &:before {
+          content: '';
+          position: absolute;
+          top: 50%;
+          right: 16px;
+          margin-top: -3px;
+          border-style: solid;
+          border-color: ${theme.colors.depthDark3} transparent transparent
+            transparent;
+          border-width: 6px 4px 0 4px;
+        }
+      }
     }
 
     & .fi-combobox_wrapper {
@@ -15,6 +31,13 @@ export const baseStyles = withSuomifiTheme(
     }
 
     &.fi-combobox--open {
+      & .fi-filter-input_input-element-container {
+        &:before {
+          border-color: transparent transparent ${theme.colors.depthDark3}
+            transparent;
+          border-width: 0 4px 6px 4px;
+        }
+      }
       & .fi-filter-input_input {
         border-bottom: 0;
         border-bottom-left-radius: 0;
