3edcc65332370ddf44f5ba3befe5d4bcbc2de599
Sami Korpela <ketsappi@users.noreply.github.com>
2021-02-11 09:52:54 +0200

    Fix item focus

--- src/core/Form/Combobox/ComboboxItem.baseStyles.tsx
+++ src/core/Form/Combobox/ComboboxItem.baseStyles.tsx
@@ -1,7 +1,7 @@
 import { css } from 'styled-components';
 import { withSuomifiTheme, TokensAndTheme } from '../../theme';
 import { font } from '../../theme/reset';
-import { boxShadowFocus } from '../../theme/utils';
+import { absoluteFocus } from '../../theme/utils';
 
 export const baseStyles = withSuomifiTheme(
   ({ theme }: TokensAndTheme) => css`
@@ -11,7 +11,12 @@ export const baseStyles = withSuomifiTheme(
       }
 
       &--currentSelection {
-        ${boxShadowFocus}
+        outline: 0;
+        position: relative;
+
+        &::after {
+          ${absoluteFocus}
+        }
       }
 
       & .fi-checkbox {
