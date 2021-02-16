1252a940098f4c0633e6c4677576dfb78a6bf5e9
Aappo Ålander <aappo.alander@gofore.com>
2021-02-16 15:49:02 +0200

    Remove tokens prop from checkbox.

--- src/core/Form/Checkbox/Checkbox.baseStyles.tsx
+++ src/core/Form/Checkbox/Checkbox.baseStyles.tsx
@@ -1,15 +1,11 @@
 import { css } from 'styled-components';
-import {
-  withSuomifiTheme,
-  TokensAndTheme,
-  SuomifiThemeProp,
-} from '../../theme';
+import { defaultThemeTokens as theme } from '../../theme';
 import { disabledCursor } from '../../../components/utils/css';
 import { element, font } from '../../theme/reset';
 import { boxShadowFocus } from '../../theme/utils/focus';
 
 /* stylelint-disable no-descending-specificity */
-const checkedStyles = ({ theme }: SuomifiThemeProp) => css`
+const checkedStyles = css`
   &.fi-checkbox--checked {
     & .fi-checkbox_label {
       &::before {
@@ -22,7 +18,7 @@ const checkedStyles = ({ theme }: SuomifiThemeProp) => css`
   }
 `;
 
-const disabledStyles = ({ theme }: SuomifiThemeProp) => css`
+const disabledStyles = css`
   &.fi-checkbox--disabled {
     & .fi-checkbox_label {
       ${disabledCursor}
@@ -44,7 +40,7 @@ const disabledStyles = ({ theme }: SuomifiThemeProp) => css`
   }
 `;
 
-const errorStyles = ({ theme }: SuomifiThemeProp) => css`
+const errorStyles = css`
   &.fi-checkbox--error {
     & .fi-checkbox_label {
       &::before {
@@ -58,7 +54,7 @@ const errorStyles = ({ theme }: SuomifiThemeProp) => css`
   }
 `;
 
-const largeVariantStyles = ({ theme }: SuomifiThemeProp) => css`
+const largeVariantStyles = css`
   &.fi-checkbox--large {
     & .fi-checkbox_label {
       padding-left: ${theme.spacing.xxl};
@@ -88,68 +84,66 @@ const largeVariantStyles = ({ theme }: SuomifiThemeProp) => css`
   }
 `;
 
-export const baseStyles = withSuomifiTheme(
-  ({ theme }: TokensAndTheme) => css`
-    ${element({ theme })}
-    ${font({ theme })('bodyText')}
+export const baseStyles = css`
+  ${element({ theme })}
+  ${font({ theme })('bodyText')}
 
     & .fi-checkbox_label {
-      position: relative;
-      display: block;
-      padding-left: ${theme.spacing.l};
-      cursor: pointer;
-      min-height: 27px;
-      line-height: 1.5em;
-      padding-top: 1px;
+    position: relative;
+    display: block;
+    padding-left: ${theme.spacing.l};
+    cursor: pointer;
+    min-height: 27px;
+    line-height: 1.5em;
+    padding-top: 1px;
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0px;
+      top: ${theme.spacing.xxs};
+      box-sizing: border-box;
+      height: 18px;
+      width: 18px;
+      border: 1px solid ${theme.colors.depthDark3};
+      border-radius: ${theme.radius.basic};
+      background-color: ${theme.colors.whiteBase};
+    }
+  }
+  & .fi-checkbox_icon {
+    position: absolute;
+    height: 10px;
+    width: 10px;
+    left: 4px;
+    top: 9px;
+  }
+
+  &:focus-within {
+    & .fi-checkbox_label {
       &::before {
-        content: '';
-        position: absolute;
-        left: 0px;
-        top: ${theme.spacing.xxs};
-        box-sizing: border-box;
-        height: 18px;
-        width: 18px;
-        border: 1px solid ${theme.colors.depthDark3};
-        border-radius: ${theme.radius.basic};
-        background-color: ${theme.colors.whiteBase};
+        ${boxShadowFocus}
       }
     }
-    & .fi-checkbox_icon {
-      position: absolute;
-      height: 10px;
-      width: 10px;
-      left: 4px;
-      top: 9px;
-    }
+  }
 
-    &:focus-within {
-      & .fi-checkbox_label {
-        &::before {
-          ${boxShadowFocus}
-        }
-      }
-    }
+  & .fi-checkbox_input {
+    position: absolute;
+    opacity: 0;
+    z-index: -9999;
+  }
 
-    & .fi-checkbox_input {
-      position: absolute;
-      opacity: 0;
-      z-index: -9999;
-    }
+  & .fi-hint-text {
+    padding-left: ${theme.spacing.l};
+    color: ${theme.colors.depthDark1};
+    margin-bottom: 0;
+  }
 
-    & .fi-hint-text {
-      padding-left: ${theme.spacing.l};
-      color: ${theme.colors.depthDark1};
-      margin-bottom: 0;
-    }
+  & .fi-status-text {
+    display: block;
+    line-height: 18px;
+  }
 
-    & .fi-status-text {
-      display: block;
-      line-height: 18px;
-    }
-
-    ${largeVariantStyles({ theme })};
-    ${checkedStyles({ theme })};
-    ${errorStyles({ theme })};
-    ${disabledStyles({ theme })};
-  `,
-);
+  ${largeVariantStyles};
+  ${checkedStyles};
+  ${errorStyles};
+  ${disabledStyles};
+`;
