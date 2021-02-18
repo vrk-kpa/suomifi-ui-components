de3620af5a9c3381cc98d887a566bbd877876fb6
Aappo Ålander <aappo.alander@gofore.com>
2021-02-18 17:11:30 +0200

    Remove tokens prop from Colors.

--- src/core/Colors/Colors.baseStyles.tsx
+++ src/core/Colors/Colors.baseStyles.tsx
@@ -1,75 +1,73 @@
 import { css } from 'styled-components';
 import { readableColor } from 'polished';
-import { withSuomifiTheme, TokensAndTheme } from '../theme';
+import { defaultThemeTokens as theme } from '../theme';
 import { clearfix } from '../../utils/css/utils';
 import { ColorProps } from './Colors';
 
-export const baseStyles = withSuomifiTheme(
-  ({ theme, color: colorProp }: TokensAndTheme & Partial<ColorProps>) => {
-    const color = !!colorProp ? colorProp : theme.colors.blackBase;
-    return css`
-      ${theme.typography.bodyText}
+export const baseStyles = ({ color: colorProp }: Partial<ColorProps>) => {
+  const color = !!colorProp ? colorProp : theme.colors.blackBase;
+  return css`
+    ${theme.typography.bodyText}
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    float: left;
+    width: 208px;
+    height: 196px;
+    max-width: 100%;
+    color: ${readableColor(color)};
+    border-bottom: 4px solid ${color};
+    cursor: pointer;
+    z-index: 2;
+    position: relative;
+    margin: 0;
+
+    &:hover {
+      border-bottom-color: ${readableColor(color)};
+    }
+
+    figcaption {
       display: flex;
       flex-direction: column;
       justify-content: flex-end;
-      float: left;
-      width: 208px;
-      height: 196px;
-      max-width: 100%;
-      color: ${readableColor(color)};
-      border-bottom: 4px solid ${color};
-      cursor: pointer;
-      z-index: 2;
-      position: relative;
-      margin: 0;
+      position: absolute;
+      left: ${theme.spacing.insetM};
+      bottom: ${theme.spacing.insetM};
+    }
 
-      &:hover {
-        border-bottom-color: ${readableColor(color)};
-      }
+    svg {
+      width: 100%;
+      height: 100%;
 
-      figcaption {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-        position: absolute;
-        left: ${theme.spacing.insetM};
-        bottom: ${theme.spacing.insetM};
-      }
-
-      svg {
+      rect {
         width: 100%;
         height: 100%;
+      }
+    }
 
-        rect {
-          width: 100%;
-          height: 100%;
-        }
+    .fi-color_name {
+      position: relative;
+      width: 100%;
+      text-align: right;
+      overflow-wrap: break-word;
+      z-index: 3;
+      pointer-events: none;
+
+      &--hex {
+        ${theme.typography.bodyTextSmall}
+        opacity: .4;
       }
 
-      .fi-color_name {
-        position: relative;
-        width: 100%;
-        text-align: right;
-        overflow-wrap: break-word;
-        z-index: 3;
-        pointer-events: none;
-
-        &--hex {
-          ${theme.typography.bodyTextSmall}
-          opacity: .4;
-        }
-
-        &--key {
-          ${theme.typography.bodySemiBold}
-        }
+      &--key {
+        ${theme.typography.bodySemiBold}
       }
+    }
 
-      &:hover .fi-color_name--key {
-        text-decoration: underline;
-      }
-    `;
-  },
-);
+    &:hover .fi-color_name--key {
+      text-decoration: underline;
+    }
+  `;
+};
 
 export const containerStyles = css`
   ${clearfix}
