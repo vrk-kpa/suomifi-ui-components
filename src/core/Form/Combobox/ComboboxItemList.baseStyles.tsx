49e82bdca6098d3b7b22fe6c7e8981ac188139f4
Sami Korpela <ketsappi@users.noreply.github.com>
2021-02-22 13:55:18 +0200

    Move item focus styles to parent level with Combobox

--- src/core/Form/Combobox/ComboboxItemList.baseStyles.tsx
+++ src/core/Form/Combobox/ComboboxItemList.baseStyles.tsx
@@ -1,6 +1,7 @@
 import { css } from 'styled-components';
 import { withSuomifiTheme, TokensAndTheme } from '../../theme';
 import { font } from '../../theme/reset';
+import { absoluteFocus } from '../../theme/utils';
 
 export const baseStyles = withSuomifiTheme(
   ({ theme }: TokensAndTheme) => css`
@@ -12,6 +13,16 @@ export const baseStyles = withSuomifiTheme(
 
       &:focus {
         outline: none;
+      }
+      &:focus-visible {
+        & .fi-combobox-item--currentSelection {
+          outline: 0;
+          position: relative;
+
+          &::after {
+            ${absoluteFocus}
+          }
+        }
       }
     }
 
