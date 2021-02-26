8e5759ea9216139e43491f3fe6ec1892cf81f2b7
Sami Korpela <ketsappi@users.noreply.github.com>
2021-02-26 10:17:51 +0200

    Remove tokens prop support from Combobox

--- src/core/Form/Combobox/ChipList/ChipList.baseStyles.tsx
+++ src/core/Form/Combobox/ChipList/ChipList.baseStyles.tsx
@@ -1,19 +1,17 @@
 import { css } from 'styled-components';
-import { withSuomifiTheme, TokensAndTheme } from '../../../theme';
+import { defaultThemeTokens as theme } from '../../../theme';
 import { font } from '../../../theme/reset';
 
-export const baseStyles = withSuomifiTheme(
-  ({ theme }: TokensAndTheme) => css`
-    &.fi-chip-list {
-      ${font({ theme })('bodyText')}
-      padding-top: 15px;
-    }
+export const baseStyles = css`
+  &.fi-chip-list {
+    ${font(theme)('bodyText')}
+    padding-top: 15px;
+  }
 
-    & .fi-chip-list_wrapper {
-      display: inline-flex;
-      flex-wrap: wrap;
-      width: 100%;
-      gap: 10px;
-    }
-  `,
-);
+  & .fi-chip-list_wrapper {
+    display: inline-flex;
+    flex-wrap: wrap;
+    width: 100%;
+    gap: 10px;
+  }
+`;
