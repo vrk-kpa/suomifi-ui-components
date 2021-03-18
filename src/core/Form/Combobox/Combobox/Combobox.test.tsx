7ee1c8243520c79eacd2fb2dbd5c256e305b9ee8
Sami Korpela <ketsappi@users.noreply.github.com>
2021-03-18 10:33:07 +0200

    Add tests for Combobox

---
+++ src/core/Form/Combobox/Combobox/Combobox.test.tsx
@@ -0,0 +1,136 @@
+import React from 'react';
+// import { render, prettyDOM } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { axeTest } from '../../../../utils/test/axe';
+import { Combobox } from './Combobox';
+
+const tools = [
+  {
+    name: 'Jackhammer',
+    price: 230,
+    tax: false,
+    labelText: 'Jackhammer',
+    uniqueItemId: 'jh2435626',
+  },
+  {
+    name: 'Hammer',
+    price: 15,
+    tax: true,
+    labelText: 'Hammer',
+    uniqueItemId: 'h9823523',
+  },
+  {
+    name: 'Sledgehammer',
+    price: 36,
+    tax: false,
+    labelText: 'Sledgehammer',
+    uniqueItemId: 'sh908293482',
+  },
+  {
+    name: 'Spade',
+    price: 50,
+    tax: true,
+    labelText: 'Spade',
+    uniqueItemId: 's82502335',
+  },
+  {
+    name: 'Powersaw',
+    price: 150,
+    tax: false,
+    labelText: 'Powersaw',
+    disabled: true,
+    uniqueItemId: 'ps9081231',
+  },
+  {
+    name: 'Shovel',
+    price: 115,
+    tax: true,
+    labelText: 'Shovel',
+    uniqueItemId: 's05111511',
+  },
+  {
+    name: 'Iron stick',
+    price: 85,
+    tax: false,
+    labelText: 'Iron stick',
+    uniqueItemId: 'is3451261',
+  },
+  {
+    name: 'Rake',
+    price: 50,
+    tax: true,
+    labelText: 'Rake',
+    uniqueItemId: 'r09282626',
+  },
+  {
+    name: 'Motorsaw',
+    price: 450,
+    tax: false,
+    labelText: 'Motorsaw',
+    disabled: true,
+    uniqueItemId: 'ms6126266',
+  },
+];
+
+const defaultSelectedTools = [
+  {
+    name: 'Hammer',
+    price: 15,
+    tax: true,
+    labelText: 'Hammer',
+    uniqueItemId: 'h9823523',
+  },
+  {
+    name: 'Powersaw',
+    price: 150,
+    tax: false,
+    labelText: 'Powersaw',
+    disabled: true,
+    uniqueItemId: 'ps9081231',
+  },
+];
+
+const BasicCombobox = (
+  <Combobox
+    labelText="Combobox"
+    items={tools}
+    chipListVisible={true}
+    chipActionLabel="Remove"
+    removeAllButtonLabel="Remove all selections"
+    visualPlaceholder="Choose your tool(s)"
+    emptyItemsLabel="No items"
+    defaultSelectedItems={defaultSelectedTools}
+  />
+);
+
+test('should not have basic accessibility issues', async () =>
+  axeTest(BasicCombobox));
+
+describe('Chips', () => {
+  it('should have selected Chips shown', () => {
+    const { container } = render(BasicCombobox);
+    expect(container.querySelectorAll('.fi-chip').length).toEqual(2);
+  });
+
+  it('first Chip should be removable and removed when clicked', async () => {
+    const promise = Promise.resolve();
+    const { container } = render(BasicCombobox);
+    const chip = container.querySelectorAll('.fi-chip')[0];
+    expect(chip).toHaveTextContent('Hammer');
+    expect(chip.querySelector('.fi-chip--icon')).not.toBeNull();
+    fireEvent.click(chip, {});
+    await act(() => promise);
+    expect(container.querySelectorAll('.fi-chip').length).toEqual(1);
+    const disabledChip = container.querySelectorAll('.fi-chip')[0];
+    expect(disabledChip).toHaveTextContent('Powersaw');
+  });
+
+  it('second Chip should be disabled', () => {
+    const { container } = render(BasicCombobox);
+    const disabledChip = container.querySelectorAll('.fi-chip')[1];
+    expect(disabledChip).toHaveTextContent('Powersaw');
+    expect(disabledChip).toHaveClass('fi-chip--disabled');
+    expect(disabledChip).toHaveAttribute('disabled');
+  });
+});
