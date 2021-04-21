2c9c2ae55dfb87cafa9be0099df816edb72b6d6d
Sami Korpela <ketsappi@users.noreply.github.com>
2021-04-21 15:01:18 +0300

    Change Combobox popover to not use inline JS for styling
    Background color is now done in the item list's baseStyle

--- src/core/Form/Combobox/ComboboxItemList/ComboboxItemList.baseStyles.tsx
+++ src/core/Form/Combobox/ComboboxItemList/ComboboxItemList.baseStyles.tsx
@@ -7,6 +7,7 @@ export const baseStyles = css`
     ${font(theme)('bodyText')}
     list-style-type: none;
 
+    background-color: ${theme.colors.whiteBase};
     border-width: 0 1px 1px 1px;
     border-style: solid;
     border-color: ${theme.colors.depthDark3};
