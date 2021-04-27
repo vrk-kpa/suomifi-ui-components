9b91344b0ad7b5974914c304c3ce0c2b72c53a0b
Sami Korpela <ketsappi@users.noreply.github.com>
2021-04-27 13:24:51 +0300

    Change to use padding+margin combination instead of gap
    Gap is not so well supported at the time

--- src/core/Form/Combobox/ChipList/ChipList.baseStyles.tsx
+++ src/core/Form/Combobox/ChipList/ChipList.baseStyles.tsx
@@ -5,13 +5,18 @@ import { font } from '../../../theme/reset';
 export const baseStyles = css`
   &.fi-chip-list {
     ${font(theme)('bodyText')}
-    padding-top: 15px;
+    padding-top: 5px;
+  }
+
+  & .fi-chip-list_content_wrapper > * {
+    margin-right: 10px;
+    margin-top: 10px;
+    padding-right: -10px;
   }
 
   & .fi-chip-list_content_wrapper {
     display: inline-flex;
     flex-wrap: wrap;
     width: 100%;
-    gap: 10px;
   }
 `;
