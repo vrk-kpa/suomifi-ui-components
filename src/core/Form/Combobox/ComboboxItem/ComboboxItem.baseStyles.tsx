3ed5f4b6fc8107a3b61211a5cd6105dd0c76e39e
Sami Korpela <ketsappi@users.noreply.github.com>
2021-05-11 11:47:14 +0300

    Change ComboboxItem structure to fix selection problem with mobile devices

--- src/core/Form/Combobox/ComboboxItem/ComboboxItem.baseStyles.tsx
+++ src/core/Form/Combobox/ComboboxItem/ComboboxItem.baseStyles.tsx
@@ -8,57 +8,95 @@ export const baseStyles = css`
       outline: none;
     }
 
-    & .fi-checkbox {
-      padding: 10px;
-      user-select: none;
-      ${font(theme)('actionElementInnerText')}
+    padding: 10px;
+    ${font(theme)('actionElementInnerText')}
 
-      & .fi-combobox-item--query_highlight {
-        background-color: transparent;
-        font-weight: bold;
+    & .fi-combobox-item--query_highlight {
+      background-color: transparent;
+      font-weight: bold;
+    }
+    & .fi-combobox-item_text {
+      position: relative;
+      padding-left: ${theme.spacing.l};
+      min-height: 27px;
+      line-height: 1.5em;
+      padding-top: 1px;
+      &::before {
+        content: '';
+        position: absolute;
+        left: 0px;
+        top: ${theme.spacing.xxs};
+        box-sizing: border-box;
+        height: 18px;
+        width: 18px;
+        border: 1px solid ${theme.colors.depthDark3};
+        border-radius: ${theme.radius.basic};
+        background-color: ${theme.colors.whiteBase};
       }
 
-      & .fi-checkbox_icon {
+      & .fi-combobox-item_checkbox_icon {
         pointer-events: none;
+        position: absolute;
+        height: 10px;
+        width: 10px;
+        left: 4px;
+        top: 9px;
       }
     }
 
-    & .fi-checkbox--checked {
+    &--selected {
       background-color: ${theme.colors.depthSecondaryDark1};
       color: ${theme.colors.blackBase};
 
+      & > .fi-combobox-item_checkbox_icon {
+        position: absolute;
+        fill: ${theme.colors.highlightBase};
+      }
+
+      & .fi-combobox-item_text {
+        &::before {
+          border-color: ${theme.colors.highlightBase};
+        }
+        & > .fi-combobox-item_checkbox_icon {
+          fill: ${theme.colors.highlightBase};
+        }
+      }
       & .fi-combobox-item--query_highlight {
         color: ${theme.colors.blackBase};
       }
     }
 
-    &--hasKeyboardFocus {
-      & .fi-checkbox {
-        background-color: ${theme.colors.highlightBase};
-        color: ${theme.colors.whiteBase};
-
-        & .fi-combobox-item--query_highlight {
-          color: ${theme.colors.whiteBase};
+    &--disabled {
+      & .fi-combobox-item_text {
+        cursor: not-allowed;
+        color: ${theme.colors.depthBase};
+        &::before {
+          background-color: ${theme.colors.depthLight3};
+          border-color: ${theme.colors.depthLight1};
+          border-width: 1px;
         }
-
-        &.fi-checkbox--checked {
-          background-color: ${theme.colors.highlightBase};
-          color: ${theme.colors.whiteBase};
-
-          & .fi-combobox-item--query_highlight {
-            color: ${theme.colors.whiteBase};
-          }
+        & > .fi-combobox-item_checkbox_icon {
+          fill: ${theme.colors.depthLight1};
         }
       }
     }
 
-    &:hover {
-      & .fi-checkbox {
-        background-color: ${theme.colors.highlightBase};
+    &--hasKeyboardFocus {
+      background-color: ${theme.colors.highlightBase};
+      color: ${theme.colors.whiteBase};
+
+      & .fi-combobox-item--query_highlight {
         color: ${theme.colors.whiteBase};
-        & .fi-combobox-item--query_highlight {
-          color: ${theme.colors.whiteBase};
-        }
+
+        background-color: ${theme.colors.highlightBase};
+      }
+    }
+
+    &:hover {
+      background-color: ${theme.colors.highlightBase};
+      color: ${theme.colors.whiteBase};
+      & .fi-combobox-item--query_highlight {
+        color: ${theme.colors.whiteBase};
       }
     }
   }
