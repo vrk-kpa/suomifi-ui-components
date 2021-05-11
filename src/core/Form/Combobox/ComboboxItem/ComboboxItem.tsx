53016af08f14c94f194a11a8995598aaaad02513
Sami Korpela <ketsappi@users.noreply.github.com>
2021-05-11 11:47:14 +0300

    Change ComboboxItem structure to fix selection problem with mobile devices

--- src/core/Form/Combobox/ComboboxItem/ComboboxItem.tsx
+++ src/core/Form/Combobox/ComboboxItem/ComboboxItem.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { default as styled } from 'styled-components';
 import classnames from 'classnames';
-import { HtmlDiv, HtmlLi } from '../../../../reset';
-import { CheckboxProps, Checkbox } from '../../Checkbox/Checkbox';
+import { HtmlLi, HtmlSpan } from '../../../../reset';
+import { Icon } from '../../../Icon/Icon';
 import { baseStyles } from './ComboboxItem.baseStyles';
 
 const baseClassName = 'fi-combobox-item';
@@ -10,14 +10,27 @@ const baseClassName = 'fi-combobox-item';
 const comboboxItemClassNames = {
   wrapper: `${baseClassName}_wrapper`,
   hasKeyboardFocus: `${baseClassName}--hasKeyboardFocus`,
+  selected: `${baseClassName}--selected`,
+  disabled: `${baseClassName}--disabled`,
+  text: `${baseClassName}_text`,
 };
 
-export interface ComboboxItemProps
-  extends Omit<CheckboxProps, 'ref' | 'forwardedRef'> {
+const iconBaseClassName = `${baseClassName}_checkbox_icon`;
+const iconClassnames = {
+  disabled: `${iconBaseClassName}--disabled`,
+  checked: `${iconBaseClassName}--checked`,
+};
+
+export interface ComboboxItemProps {
   /** ComboboxItem container div class name for custom styling. */
   className?: string;
   /** Indicates if the current item has keyboard focus. */
   hasKeyboardFocus: boolean;
+
+  checked: boolean;
+  disabled?: boolean;
+  id?: string;
+  onClick: () => void;
 }
 
 class BaseComboboxItem extends Component<ComboboxItemProps> {
@@ -29,24 +42,42 @@ class BaseComboboxItem extends Component<ComboboxItemProps> {
       hasKeyboardFocus,
       disabled,
       id,
+      onClick,
       ...passProps
     } = this.props;
+
+    const CheckedIcon = () => (
+      <Icon
+        icon="check"
+        className={classnames(iconBaseClassName, {
+          [iconClassnames.checked]: checked && !disabled,
+          [iconClassnames.disabled]: !!disabled,
+        })}
+      />
+    );
+
     return (
       <HtmlLi
         className={classnames(baseClassName, className, {
           [comboboxItemClassNames.hasKeyboardFocus]: hasKeyboardFocus,
+          [comboboxItemClassNames.selected]: checked,
+          [comboboxItemClassNames.disabled]: disabled,
         })}
         tabIndex={-1}
         role="option"
-        aria-selected={checked}
+        aria-selected={disabled ? undefined : checked}
         aria-disabled={disabled || false}
         id={id}
+        onClick={onClick}
       >
-        <HtmlDiv className={comboboxItemClassNames.wrapper}>
-          <Checkbox disabled={disabled} checked={checked} {...passProps}>
-            {children}
-          </Checkbox>
-        </HtmlDiv>
+        <HtmlSpan
+          aria-hidden={true}
+          className={comboboxItemClassNames.text}
+          {...passProps}
+        >
+          {!!checked && <CheckedIcon />}
+        </HtmlSpan>
+        {children}
       </HtmlLi>
     );
   }
