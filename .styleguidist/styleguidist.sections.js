0ced5d3361049063b0488ccc28a4916299a58225
LJKaski <joonas.kaski@gofore.com>
2021-07-08 13:20:59 +0300

    update styleguidist sections

--- .styleguidist/styleguidist.sections.js
+++ .styleguidist/styleguidist.sections.js
@@ -89,8 +89,9 @@ module.exports = {
             {
               name: 'Link',
               components: getComponentWithVariants('Link')([
-                'LinkSkip',
-                'LinkExternal',
+                'Link/Link',
+                'LinkSkip/LinkSkip',
+                'LinkExternal/LinkExternal',
               ]),
             },
             {
