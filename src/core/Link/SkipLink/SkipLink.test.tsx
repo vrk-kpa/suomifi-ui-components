2e074a789b2e92e1bb5527583202e97e101f3011
LJKaski <joonas.kaski@gofore.com>
2021-07-09 10:06:58 +0300

    update SkipLink test

--- src/core/Link/SkipLink/SkipLink.test.tsx
+++ src/core/Link/SkipLink/SkipLink.test.tsx
@@ -6,17 +6,15 @@ import { SkipLink } from './SkipLink';
 
 const TestSkipLink = (
   <SkipLink href="/" data-testid="test-link">
-    Hey this is test
+    Test content
   </SkipLink>
 );
 
-test('calling render with the same component on the same container does not remount', () => {
+test('should match snapshot', () => {
   const LinkRendered = render(TestSkipLink);
   const { getByTestId, container } = LinkRendered;
   expect(container.firstChild).toMatchSnapshot();
-  expect(getByTestId('test-link').textContent).toBe(
-    'Hey this is testOpens in a new window',
-  );
+  expect(getByTestId('test-link').textContent).toBe('Test content');
 });
 
 test('should not have basic accessibility issues', axeTest(TestSkipLink));
