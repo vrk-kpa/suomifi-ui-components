e47c9c4ffebc6aef0df2f6e96d739741b2291aa9
Aappo Ålander <aappo.alander@gofore.com>
2021-08-16 16:49:27 +0300

    Refactor breadcrumbs and remove variant prop.

--- src/core/Breadcrumb/Breadcrumb.baseStyles.ts
+++ src/core/Breadcrumb/Breadcrumb.baseStyles.ts
@@ -1,38 +1,21 @@
 import { css } from 'styled-components';
 import { SuomifiTheme } from '../theme';
-import { cssValueToString } from '../../utils/css';
 import { nav, list, listItem, font } from '../theme/reset';
 
 export const baseStyles = (theme: SuomifiTheme) => css`
   ${nav(theme)}
   ${font(theme)('bodyTextSmall')}
-  background-color: ${theme.colors.whiteBase};
   height: 1.5em;
 
   & .fi-breadcrumb {
     &_list {
       ${list(theme)}
-      ${font(theme)('bodyTextSmall')}
       margin: 0;
       padding: 0;
     }
     &_item {
       ${listItem(theme)}
-      ${font(theme)('bodyTextSmall')}
       float: left;
-      color: ${theme.colors.depthDark1};
-    }
-    &_item,
-    &_link,
-    &_icon {
-      font-size: ${cssValueToString(
-        theme.values.typography.bodyTextSmall.fontSize,
-      )};
-    }
-    &_icon {
-      transform: translateY(0.2em);
-      margin: 0 ${theme.spacing.insetXs};
-      fill: ${theme.colors.depthDark1};
     }
   }
 `;
--- src/core/Breadcrumb/BreadcrumbLink.tsx
+++ src/core/Breadcrumb/BreadcrumbLink.tsx
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { default as styled } from 'styled-components';
 import { Link, LinkProps } from '../Link';
 import { Icon } from '../Icon/Icon';
 import { HtmlSpan } from '../../reset';
 import classnames from 'classnames';
+import { baseStyles } from './BreadcrumbLink.baseStyles';
+import { SuomifiThemeConsumer, SuomifiThemeProp } from '../theme';
 
-const baseClassName = 'fi-breadcrumb';
-const linkClassName = `${baseClassName}_link`;
-const iconClassName = `${baseClassName}_icon`;
+const baseClassName = 'fi-breadcrumb-link';
+const breadcrumbClassNames = {
+  link: `${baseClassName}_link`,
+  current: `${baseClassName}_link--current`,
+  icon: `${baseClassName}_icon`,
+};
 
 export interface BreadcrumbLinkProps extends Omit<LinkProps, 'href'> {
   /** Indicating the link is the current page */
@@ -15,31 +21,51 @@ export interface BreadcrumbLinkProps extends Omit<LinkProps, 'href'> {
   href?: string;
 }
 
-export const BreadcrumbLink = ({
-  current,
-  children,
-  className,
-  href = '',
-  ...passProps
-}: BreadcrumbLinkProps) => {
-  if (!!current) {
-    return (
-      <HtmlSpan className={className} aria-current="location">
-        {children}
-      </HtmlSpan>
-    );
-  }
-
+const BaseBreadcrumbLink = (props: BreadcrumbLinkProps & SuomifiThemeProp) => {
+  const {
+    current,
+    children,
+    className,
+    theme,
+    href = '',
+    ...passProps
+  } = props;
   return (
-    <Fragment>
-      <Link
-        {...passProps}
-        className={classnames(linkClassName, className)}
-        href={href}
-      >
-        {children}
-      </Link>
-      <Icon icon="linkBreadcrumb" className={iconClassName} />
-    </Fragment>
+    <HtmlSpan className={classnames(className, baseClassName)}>
+      {!current ? (
+        <>
+          <Link
+            {...passProps}
+            className={breadcrumbClassNames.link}
+            href={href}
+          >
+            {children}
+          </Link>
+          <Icon icon="linkBreadcrumb" className={breadcrumbClassNames.icon} />
+        </>
+      ) : (
+        <HtmlSpan
+          className={classnames(
+            breadcrumbClassNames.link,
+            breadcrumbClassNames.current,
+          )}
+          aria-current="location"
+        >
+          {children}
+        </HtmlSpan>
+      )}
+    </HtmlSpan>
   );
 };
+
+const StyledBreadcrumbLink = styled(BaseBreadcrumbLink)`
+  ${({ theme }) => baseStyles(theme)}
+`;
+
+export const BreadcrumbLink = (props: BreadcrumbLinkProps) => (
+  <SuomifiThemeConsumer>
+    {({ suomifiTheme }) => (
+      <StyledBreadcrumbLink theme={suomifiTheme} {...props} />
+    )}
+  </SuomifiThemeConsumer>
+);
