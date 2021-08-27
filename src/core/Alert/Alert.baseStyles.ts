7d1f5389cee1b99c7b70fefac9d3fc8e15a2804d
LJKaski <joonas.kaski@gofore.com>
2021-08-27 16:08:21 +0300

    add preliminary styling and examples

--- src/core/Alert/Alert.baseStyles.ts
+++ src/core/Alert/Alert.baseStyles.ts
@@ -3,16 +3,62 @@ import { SuomifiTheme } from '../theme';
 import { element, font } from '../theme/reset';
 
 export const baseStyles = (theme: SuomifiTheme) => css`
+  ${element(theme)}
+  ${font(theme)('bodyText')}
+
   &.fi-alert {
-    border-left: 5px solid ${theme.colors.alertBase};
-    padding-left: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: ${theme.colors.alertLight1};
+    min-height: 55px;
+    width: 900px;
   }
 
   & .fi-alert-label {
+    padding-left: 40px;
+    margin-top: ${theme.spacing.insetXxl};
+    margin-bottom: ${theme.spacing.insetXs};
     font-weight: 600;
   }
 
-  ${element(theme)}
-  ${font(theme)('bodyText')}
+  & .fi-alert-icon {
+    margin-left: 10px;
+    vertical-align: bottom;
+    height: 24px;
+    width: 24px;
+  }
+
+  & .fi-alert-content {
+    padding-left: 40px;
+    text-align: baseline;
+  }
+
+  & .fi-alert-content-wrapper {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: calc(100% * (2 / 3));
+    padding: 5px;
+  }
+
+  & .fi-alert-text-content-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  & .fi-alert-close-button {
+    margin-left: auto;
+    & .fi-icon {
+      vertical-align: bottom;
+      width: 14px;
+      height: 14px;
+      margin-left: 7px;
+    }
+  }
+
+  &.fi-alert--inline {
+    border-left: 5px solid ${theme.colors.alertBase};
+    min-height: 64px;
+  }
 `;
--- src/core/Alert/Alert.tsx
+++ src/core/Alert/Alert.tsx
@@ -16,40 +16,47 @@ const alertClassNames = {
   content: `${baseClassName}-content`,
   label: `${baseClassName}-label`,
   icon: `${baseClassName}-icon`,
+  closeButton: `${baseClassName}-close-button`,
+  contentWrapper: `${baseClassName}-content-wrapper`,
+  textContentWrapper: `${baseClassName}-text-content-wrapper`,
 };
 
 export interface AlertProps extends HtmlSpanProps {
-  /** Show icon */
-  icon?: true;
   /** Style variant. Use maintenance for scheduled events and disturbance for urgent issues
-   * @default 'maintenance'
+   * @default 'disturbance'
    */
   variant?: 'maintenance' | 'disturbance';
   /** Label for the  alert */
   labelText?: string;
+  inline?: boolean;
   children?: ReactNode;
+  closeText: string;
 }
 
 class BaseAlert extends Component<AlertProps> {
   render() {
     const {
       className,
-      variant,
-      icon,
+      variant = 'disturbance',
       labelText,
       children,
+      inline,
+      closeText,
       ...passProps
     } = this.props;
+
+    const variantIcon = variant === 'maintenance' ? 'info' : 'error';
     return (
       <HtmlDiv
         {...passProps}
         className={classnames(baseClassName, className, {
           [`${baseClassName}--${variant}`]: !!variant,
+          [`${baseClassName}--inline`]: !!inline,
         })}
       >
-        {icon && (
+        <HtmlSpan className={classnames(alertClassNames.contentWrapper)}>
           <Icon
-            icon="arrowUp"
+            icon={variantIcon}
             color={
               variant === 'maintenance'
                 ? defaultSuomifiTheme.colors.brandBase
@@ -57,12 +64,20 @@ class BaseAlert extends Component<AlertProps> {
             }
             className={classnames(alertClassNames.icon)}
           />
-        )}
-        <HtmlDiv className={classnames(alertClassNames.label)}>
-          {labelText}
-        </HtmlDiv>
-        <HtmlSpan className={classnames(alertClassNames.content)}>
-          {children}
+          <HtmlDiv className={classnames(alertClassNames.textContentWrapper)}>
+            {labelText && (
+              <HtmlSpan className={classnames(alertClassNames.label)}>
+                {labelText}
+              </HtmlSpan>
+            )}
+            <HtmlSpan className={classnames(alertClassNames.content)}>
+              {children}
+            </HtmlSpan>
+          </HtmlDiv>
+          <HtmlDiv className={classnames(alertClassNames.closeButton)}>
+            {closeText}
+            <Icon icon="close" />
+          </HtmlDiv>
         </HtmlSpan>
       </HtmlDiv>
     );
