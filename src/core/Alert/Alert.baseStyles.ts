5e1d6d888c42c667eee7396591fe07962f9df12c
LJKaski <joonas.kaski@gofore.com>
2021-09-23 10:05:00 +0300

    add basic implementation of alert variants using flexbox

--- src/core/Alert/Alert.baseStyles.ts
+++ src/core/Alert/Alert.baseStyles.ts
@@ -4,61 +4,101 @@ import { element, font } from '../theme/reset';
 
 export const baseStyles = (theme: SuomifiTheme) => css`
   ${element(theme)}
-  ${font(theme)('bodyText')}
+  ${font(theme)('bodyTextSmall')}
 
   &.fi-alert {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: ${theme.colors.alertLight1};
-    min-height: 55px;
-    width: 900px;
-  }
+    align-items: flex-start;
+    padding: 16px 10px 15px 10px;
+    width: 100%;
 
-  & .fi-alert-label {
-    padding-left: 40px;
-    margin-top: ${theme.spacing.insetXxl};
-    margin-bottom: ${theme.spacing.insetXs};
-    font-weight: 600;
-  }
+    & .fi-alert-icon-wrapper {
+      display: flex;
+      flex: 1 0 auto;
+      justify-content: flex-end;
 
-  & .fi-alert-icon {
-    margin-left: 10px;
-    vertical-align: bottom;
-    height: 24px;
-    width: 24px;
-  }
-
-  & .fi-alert-content {
-    padding-left: 40px;
-    text-align: baseline;
-  }
-
-  & .fi-alert-content-wrapper {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    width: calc(100% * (2 / 3));
-    padding: 5px;
-  }
-
-  & .fi-alert-text-content-wrapper {
-    display: flex;
-    flex-direction: column;
-  }
-
-  & .fi-alert-close-button {
-    margin-left: auto;
-    & .fi-icon {
-      vertical-align: bottom;
-      width: 14px;
-      height: 14px;
-      margin-left: 7px;
+      & .fi-alert-icon {
+        height: 24px;
+        width: 24px;
+        transform: translateY(-1px); /** Let's try to get rid of this */
+      }
     }
-  }
 
-  &.fi-alert--inline {
-    border-left: 5px solid ${theme.colors.alertBase};
-    min-height: 64px;
+    & .fi-alert-text-content-wrapper {
+      display: flex;
+      flex: 3 1 auto;
+      flex-direction: column;
+      padding: 0 20px;
+
+      & .fi-alert-label {
+        ${font(theme)('bodySemiBoldSmall')}
+      }
+
+      & .fi-alert-content {
+        vertical-align: middle;
+        ${font(theme)('bodyTextSmall')}
+      }
+    }
+
+    & .fi-alert-close-button {
+      ${font(theme)('bodyTextSmall')}
+      flex: 1 0 auto;
+      display: flex;
+      flex-wrap: nowrap;
+      & .fi-icon {
+        width: 14px;
+        height: 14px;
+        margin-left: ${theme.spacing.insetS};
+        margin-top: ${theme.spacing.insetS};
+      }
+    }
+
+    &--notification {
+      background-color: ${theme.colors.accentSecondaryLight1};
+      & .fi-alert-icon-wrapper .fi-icon .fi-icon-base-fill {
+        fill: ${theme.colors.highlightBase};
+      }
+    }
+
+    &--error {
+      background-color: ${theme.colors.alertLight1};
+      & .fi-alert-icon-wrapper .fi-icon .fi-icon-base-fill {
+        fill: ${theme.colors.alertBase};
+      }
+    }
+
+    &--warning {
+      background-color: #fff6e0; /** needs to be warningLight1 but the token is missing from the library  */
+      & .fi-alert-icon-wrapper .fi-icon .fi-icon-base-fill {
+        fill: ${theme.colors.warningBase};
+      }
+    }
+
+    /** Inline variant styles  */
+    &--inline {
+      padding: 20px 10px;
+
+      &.fi-alert--notification {
+        border-left: 5px solid ${theme.colors.accentSecondary};
+      }
+
+      &.fi-alert--error {
+        border-left: 5px solid ${theme.colors.alertBase};
+      }
+
+      &.fi-alert--warning {
+        border-left: 5px solid ${theme.colors.warningBase};
+      }
+    }
+    /** Small screen variant styles */
+    &--small-screen {
+      & .fi-alert-close-button {
+        justify-content: flex-end;
+        & .fi-icon {
+          margin-top: 0;
+          margin-right: ${theme.spacing.xxs};
+        }
+      }
+    }
   }
 `;
