2f825533fe3d1866d4e797c4c11ef1df50299ec5
Valtteri Laukkanen <valtteri.laukkanen@gofore.com>
2021-11-10 12:40:04 +0200

    Update Alert. Better commments to styling

--- src/core/Alert/InlineAlert/InlineAlert.baseStyles.ts
+++ src/core/Alert/InlineAlert/InlineAlert.baseStyles.ts
@@ -17,11 +17,11 @@ export const baseStyles = (theme: SuomifiTheme) => css`
       flex: 0;
     }
     &.fi-alert--neutral {
-      border-left: 4px solid #4698c3; /* Later add theme.colors.example styling */
+      border-left: 4px solid #4698c3; /* Later add to theme styling these colors */
     }
 
     &.fi-alert--error {
-      border-left: 4px solid #b54439; /* Later add theme.colors.example styling */
+      border-left: 4px solid #b54439; /* Later add to theme styling these colors */
     }
 
     &.fi-alert--warning {
