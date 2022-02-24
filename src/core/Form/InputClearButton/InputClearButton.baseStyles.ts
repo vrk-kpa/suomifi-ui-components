f25e09d01b32762528c285c44dbbf1ad6c818ff9
Valtteri Laukkanen <valtteri.laukkanen@gofore.com>
2022-02-24 13:43:46 +0200

    Fix InputClearButton css. Moved css from top level wrappers to base

--- src/core/Form/InputClearButton/InputClearButton.baseStyles.ts
+++ src/core/Form/InputClearButton/InputClearButton.baseStyles.ts
@@ -2,22 +2,21 @@ import { css } from 'styled-components';
 import { SuomifiTheme } from '../../theme';
 
 export const baseStyles = (theme: SuomifiTheme) => css`
-  &.fi-input-clear-button {
-    position: relative;
-    cursor: pointer;
-    pointer-events: all;
-    height: 20px;
-    width: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &:focus {
-      outline: none;
-      &:after {
-        ${theme.focus.absoluteFocus}
-      }
+  position: relative;
+  cursor: pointer;
+  pointer-events: all;
+  height: 20px;
+  width: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:focus {
+    outline: none;
+    &:after {
+      ${theme.focus.absoluteFocus}
     }
-
+  }
+  &.fi-input-clear-button {
     & .fi-input-clear-button_icon {
       width: 16px;
       height: 16px;
