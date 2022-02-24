f82a62a843df6df7d314e29fc13a63c2f86e6fba
Valtteri Laukkanen <valtteri.laukkanen@gofore.com>
2022-02-24 14:40:17 +0200

    Fix StatusText css. Moved css from top level wrappers to base

--- src/core/Form/StatusText/StatusText.baseStyles.tsx
+++ src/core/Form/StatusText/StatusText.baseStyles.tsx
@@ -3,13 +3,12 @@ import { SuomifiTheme } from '../../theme';
 import { font } from '../../theme/reset';
 
 export const baseStyles = (theme: SuomifiTheme) => css`
+  ${font(theme)('bodySemiBoldSmall')};
+  color: ${theme.colors.blackBase};
+  font-size: 14px;
+  line-height: 20px;
+  display: block;
   &.fi-status-text {
-    ${font(theme)('bodySemiBoldSmall')};
-    color: ${theme.colors.blackBase};
-    font-size: 14px;
-    line-height: 20px;
-    display: block;
-
     &.fi-status-text--error {
       color: ${theme.colors.alertBase};
     }
