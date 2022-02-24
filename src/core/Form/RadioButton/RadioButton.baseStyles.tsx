9e1de35786d9451afee32293cd6c26ba04ff9775
Valtteri Laukkanen <valtteri.laukkanen@gofore.com>
2022-02-24 14:20:29 +0200

    Fix RadioButtons css. Moved css from top level wrappers to base

--- src/core/Form/RadioButton/RadioButton.baseStyles.tsx
+++ src/core/Form/RadioButton/RadioButton.baseStyles.tsx
@@ -6,8 +6,8 @@ import { element, font } from '../../theme/reset';
 export const baseStyles = (theme: SuomifiTheme) => css`
   ${element(theme)}
   ${font(theme)('bodyText')}
-    &.fi-radio-button {
-    position: relative;
+  position: relative;
+  &.fi-radio-button {
     & .fi-radio-button_hintText {
       display: block;
       padding-left: 26px;
