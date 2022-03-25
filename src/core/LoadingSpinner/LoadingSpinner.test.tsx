ff3e8a3f9788f7e07ce3df0c791b3466903575e0
Valtteri Laukkanen <valtteri.laukkanen@gofore.com>
2022-03-25 17:28:43 +0200

    Feature LoadingSpinner. First working version of Loading Spinner

---
+++ src/core/LoadingSpinner/LoadingSpinner.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { LoadingSpinner } from './LoadingSpinner';
+import { axeTest } from '../../utils/test';
+
+describe('props', () => {
+  describe('labelMode should be visually-hidden', () => {
+    const { getByText } = render(
+      <LoadingSpinner labelText="To be hidden" labelMode="hidden" />,
+    );
+    const spinner = getByText('To be hidden');
+    expect(spinner).toHaveClass('fi-visually-hidden');
+  });
+  describe('id', () => {
+    const LoadingSpinnerWithElementId = (
+      <LoadingSpinner id="TestId" labelText="Loading data" />
+    );
+    const { container } = render(LoadingSpinnerWithElementId);
+    expect(container.querySelector('#TestId')).toHaveClass('fi-loadingSpinner');
+  });
+  it('labelAlign', () => {
+    const labelAlignLoadingSpinner = (
+      <LoadingSpinner labelAlign="right" labelText="Loading data" />
+    );
+    const { container } = render(labelAlignLoadingSpinner);
+    expect(container.firstChild).toHaveClass(
+      'fi-loadingSpinner-labelAlign-right',
+    );
+  });
+  describe('className', () => {
+    const customClassLoadingSpinner = (
+      <LoadingSpinner labelText="Loading data" className="custom-class" />
+    );
+
+    it('contains base classname', () => {
+      const { container } = render(customClassLoadingSpinner);
+      expect(container.firstChild).toHaveClass('fi-loadingSpinner');
+    });
+
+    it('contains custom classname', () => {
+      const { container } = render(customClassLoadingSpinner);
+      expect(container.firstChild).toHaveClass('custom-class');
+    });
+  });
+  it('labelText', () => {
+    const LabelTextLoadingSpinner = (
+      <LoadingSpinner labelText="Loading data" className="custom-class" />
+    );
+    const { getByText } = render(LabelTextLoadingSpinner);
+    const label = getByText('Loading data');
+    expect(label).toHaveClass('fi-loadingSpinner-label');
+  });
+});
+
+describe('accessibility', () => {
+  const TestLoadingSpinner = <LoadingSpinner labelText="Loading data" />;
+  test(
+    'should not have basic accessibility issues',
+    axeTest(TestLoadingSpinner),
+  );
+});
