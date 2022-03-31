4d83170fdc8841dee659061a82e90b3bec60540e
Sami Korpela <ketsappi@users.noreply.github.com>
2022-03-31 11:11:17 +0300

    Update jest config. Disable coverage collection
    We were not utilizing the coverage reports and it just caused unwanted
    delays for testing. It can be enabled again if there will rise actual
    need for it.

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
-  collectCoverage: true,
+  collectCoverage: false,
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
