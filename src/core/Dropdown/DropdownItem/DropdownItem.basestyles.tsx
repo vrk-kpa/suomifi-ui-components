cfc9ed8ea1a5cf06990d785e8f47891674968d26
Ilkka Pättikangas <ilkka.pattikangas@gmail.com>
2022-04-14 13:05:47 +0300

    Set cursor to pointer on Dropdown, SingleSelect & MultiSelect options lists

--- src/core/Dropdown/DropdownItem/DropdownItem.basestyles.tsx
+++ src/core/Dropdown/DropdownItem/DropdownItem.basestyles.tsx
@@ -6,6 +6,7 @@ export const baseStyles = (theme: SuomifiTheme) => css`
   &[data-reach-listbox-option].fi-dropdown_item {
     ${element(theme)}
     ${theme.typography.actionElementInnerText}
+    cursor: pointer;
     line-height: 1.5;
     padding: ${theme.spacing.insetM};
     border: 0;
