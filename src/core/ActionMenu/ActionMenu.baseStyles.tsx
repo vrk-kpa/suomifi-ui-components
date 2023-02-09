ac43288aec2ef45de6b06e53cd90c80f450867f0
Mikko Riippi <mikko.riippi@gofore.com>
2023-02-09 14:31:37 +0200

    Add ActionMenu files

---
+++ src/core/ActionMenu/ActionMenu.baseStyles.tsx
@@ -0,0 +1,140 @@
+import { css } from 'styled-components';
+import { SuomifiTheme } from '../theme';
+import { input, containerIEFocus, font } from '../theme/reset';
+
+export const baseStyles = (theme: SuomifiTheme) => css`
+  ${font(theme)('bodyText')}
+  width: 150px;
+
+  &.fi-date-input--full-width {
+    width: 100%;
+  }
+
+  & .fi-date-input_wrapper {
+    width: 100%;
+    display: inline-block;
+
+    & .fi-date-input_label--visible {
+      margin-bottom: ${theme.spacing.xs};
+    }
+
+    & .fi-hint-text {
+      margin-bottom: ${theme.spacing.xs};
+    }
+
+    & .fi-date-input_statusText--has-content {
+      margin-top: ${theme.spacing.xxs};
+    }
+  }
+
+  & .fi-date-input_input-element-container {
+    ${containerIEFocus(theme)}
+
+    &:focus-within {
+      position: relative;
+
+      &::after {
+        ${theme.focus.absoluteFocus}
+      }
+    }
+
+    width: 100%;
+  }
+
+  & .fi-date-input_picker-element-container {
+    flex: 1;
+  }
+
+  & .fi-date-input_input-and-picker-wrapper {
+    display: flex;
+  }
+
+  & .fi-date-input_input {
+    ${input(theme)}
+    background-color: ${theme.colors.whiteBase};
+    min-width: 40px;
+    width: 100%;
+    min-height: 40px;
+    padding-left: ${theme.spacing.insetL};
+    border-color: ${theme.colors.depthDark3};
+    border-radius: ${theme.radius.basic};
+
+    &::placeholder {
+      font-style: italic;
+      color: ${theme.colors.depthDark2};
+      opacity: 1;
+    }
+  }
+
+  & .fi-date-input_picker-button {
+    ${containerIEFocus(theme)}
+
+    &:focus-within {
+      position: relative;
+
+      &::after {
+        ${theme.focus.absoluteFocus}
+      }
+    }
+
+    height: 100%;
+    min-width: 40px;
+    text-align: center;
+    padding: ${theme.spacing.xs};
+    border: 1px solid ${theme.colors.highlightBase};
+    border-radius: ${theme.radius.basic};
+    &:focus {
+      outline: 3px solid transparent;
+    }
+  }
+
+  & .fi-date-input_picker-icon {
+    color: ${theme.colors.highlightBase};
+  }
+
+  & .fi-date-input_picker-button--disabled {
+    cursor: default;
+    border-color: ${theme.colors.depthBase};
+
+    .fi-date-input_picker-icon {
+      color: ${theme.colors.depthBase};
+    }
+  }
+
+  &.fi-date-input--error {
+    & .fi-date-input_input {
+      border: 2px solid ${theme.colors.alertBase};
+    }
+
+    & .fi-date-input_picker-button {
+      border: 2px solid ${theme.colors.alertBase};
+      border-left: 1px solid ${theme.colors.highlightBase};
+      border-radius: 0 ${theme.radius.basic} ${theme.radius.basic} 0;
+    }
+  }
+
+  &.fi-date-input--success {
+    & .fi-date-input_input {
+      border: 2px solid ${theme.colors.successBase};
+    }
+
+    & .fi-date-input_picker-button {
+      border: 2px solid ${theme.colors.successBase};
+      border-left: 1px solid ${theme.colors.highlightBase};
+      border-radius: 0 ${theme.radius.basic} ${theme.radius.basic} 0;
+    }
+  }
+  &.fi-date-input--disabled {
+    & .fi-date-input_input {
+      color: ${theme.colors.depthBase};
+      background-color: ${theme.colors.depthLight3};
+    }
+  }
+
+  &.fi-date-input--has-picker {
+    & .fi-date-input_input {
+      border-right: none;
+      border-radius: ${theme.radius.basic} 0 0 ${theme.radius.basic};
+    }
+  }
+`;
---
+++ src/core/ActionMenu/ActionMenu.tsx
@@ -0,0 +1,196 @@
+import React, { forwardRef, FocusEvent, useState, useRef } from 'react';
+import { default as styled } from 'styled-components';
+import classnames from 'classnames';
+import { AutoId } from '../utils/AutoId/AutoId';
+import { SuomifiThemeProp, SuomifiThemeConsumer } from '../theme';
+
+import {
+  HtmlInputProps,
+  HtmlDiv,
+  HtmlDivProps,
+  HtmlSpan,
+  HtmlButton,
+} from '../../reset';
+import { ActionMenuPopover } from './ActionMenuPopover';
+
+import { VisuallyHidden } from '../VisuallyHidden/VisuallyHidden';
+import { Icon } from '../Icon/Icon';
+
+import { baseStyles } from './ActionMenu.baseStyles';
+
+const baseClassName = 'fi-date-input';
+export const actionMenuClassNames = {
+  baseClassName,
+  fullWidth: `${baseClassName}--full-width`,
+  hasPicker: `${baseClassName}--has-picker`,
+  disabled: `${baseClassName}--disabled`,
+  error: `${baseClassName}--error`,
+  success: `${baseClassName}--success`,
+  labelIsVisible: `${baseClassName}_label--visible`,
+  inputAndPickerWrapper: `${baseClassName}_input-and-picker-wrapper`,
+  inputElementContainer: `${baseClassName}_input-element-container`,
+  inputElement: `${baseClassName}_input`,
+  pickerElementContainer: `${baseClassName}_picker-element-container`,
+  pickerButton: `${baseClassName}_picker-button`,
+  pickerButtonDisabled: `${baseClassName}_picker-button--disabled`,
+  pickerIcon: `${baseClassName}_picker-icon`,
+  styleWrapper: `${baseClassName}_wrapper`,
+  statusTextHasContent: `${baseClassName}_statusText--has-content`,
+};
+
+export interface ActionMenuProps
+  extends Omit<HtmlInputProps, 'type' | 'onChange'> {
+  /** TextInput container div class name for custom styling. */
+  className?: string;
+  /** TextInput wrapping div element props */
+  wrapperProps?: Omit<HtmlDivProps, 'className'>;
+  /** Disable input usage */
+  disabled?: boolean;
+  /** Callback fired when input is clicked. */
+  onClick?: () => void;
+  /** Callback fired when input value changes. If value can't be parsed to date, Invalid Date is returned.
+   * Invalid Date is a Date, whose time value is NaN.´
+   * @param {string} change.value Input value
+   * @param {Date} change.date Input value parsed to Date */
+
+  onChange?: (change: { date: Date }) => void;
+  /** Callback fired on input text onBlur
+   * @param {FocusEvent<HTMLInputElement>} event FocusEvent
+   */
+  onBlur?: (event: FocusEvent<HTMLInputElement>) => void;
+  /** Controlled value */
+  value?: string;
+  /** Set components width to 100% */
+  fullWidth?: boolean;
+  /** Label text for the button. */
+  openButtonLabel: string;
+  /** Label text for the button. */
+  buttonText?: string;
+
+  onClose?: () => void;
+
+  onOpen?: () => void;
+}
+
+const BaseActionMenu = (props: ActionMenuProps) => {
+  const {
+    className,
+    onChange: propOnChange,
+    wrapperProps,
+    id,
+    fullWidth,
+    forwardedRef,
+    'aria-describedby': ariaDescribedBy,
+    defaultValue,
+    value,
+    buttonText,
+    openButtonLabel,
+    children,
+    onOpen,
+    onClose,
+    ...passProps
+  } = props;
+
+  const openButtonRef = useRef<HTMLButtonElement>(null);
+  const [calendarVisible, setCalendarVisible] = useState<boolean>(false);
+
+  const toggleCalendar = (open: boolean, focus: boolean = false) => {
+    setCalendarVisible(open);
+
+    if (open && onOpen) {
+      onOpen();
+    }
+
+    if (!open && onClose) {
+      onClose();
+    }
+
+    if (!open && focus) {
+      openButtonRef.current?.focus();
+    }
+  };
+
+  return (
+    <HtmlDiv
+      {...wrapperProps}
+      className={classnames(baseClassName, className, {
+        [actionMenuClassNames.disabled]: !!passProps.disabled,
+        [actionMenuClassNames.fullWidth]: fullWidth,
+      })}
+    >
+      <HtmlSpan className={actionMenuClassNames.styleWrapper}>
+        <HtmlDiv className={actionMenuClassNames.inputAndPickerWrapper}>
+          <HtmlDiv className={actionMenuClassNames.pickerElementContainer}>
+            <>
+              <HtmlButton
+                forwardedRef={openButtonRef}
+                className={classnames(actionMenuClassNames.pickerButton, {
+                  [actionMenuClassNames.pickerButtonDisabled]:
+                    passProps.disabled,
+                })}
+                onClick={() => toggleCalendar(!calendarVisible)}
+                disabled={passProps.disabled}
+              >
+                {buttonText}
+                <VisuallyHidden>{openButtonLabel}</VisuallyHidden>
+                <Icon
+                  className={actionMenuClassNames.pickerIcon}
+                  aria-hidden={true}
+                  icon="menu"
+                />
+              </HtmlButton>
+              <ActionMenuPopover
+                sourceRef={openButtonRef}
+                openButtonRef={openButtonRef}
+                isOpen={calendarVisible}
+                onClose={(focus) => toggleCalendar(false, focus)}
+                children={children}
+              />
+            </>
+          </HtmlDiv>
+        </HtmlDiv>
+      </HtmlSpan>
+    </HtmlDiv>
+  );
+};
+
+const StyledActionMenu = styled(
+  ({ theme, ...passProps }: ActionMenuProps & SuomifiThemeProp) => (
+    <BaseActionMenu {...passProps} />
+  ),
+)`
+  ${({ theme }) => baseStyles(theme)}
+`;
+
+/**
+ * <i class="semantics" />
+ * Use for user inputting date.
+ * Can be used as an input field or with an additional DatePicker.
+ *
+ * Props other than specified explicitly are passed on to underlying input element.
+ * @component
+ */
+const ActionMenu = forwardRef<HTMLInputElement, ActionMenuProps>(
+  (props: ActionMenuProps, ref: React.Ref<HTMLInputElement>) => {
+    const { id: propId, ...passProps } = props;
+    return (
+      <SuomifiThemeConsumer>
+        {({ suomifiTheme }) => (
+          <AutoId id={propId}>
+            {(id) => (
+              <StyledActionMenu
+                theme={suomifiTheme}
+                id={id}
+                forwardedRef={ref}
+                {...passProps}
+              />
+            )}
+          </AutoId>
+        )}
+      </SuomifiThemeConsumer>
+    );
+  },
+);
+
+ActionMenu.displayName = 'ActionMenu';
+export { ActionMenu };
