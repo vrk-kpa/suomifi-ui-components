7b92825836dbfe7936f254620238abc7c8729363
Mikko Riippi <mikko.riippi@gofore.com>
2023-02-09 22:18:45 +0200

    Add components to build

--- .styleguidist/styleguidist.sections.js
+++ .styleguidist/styleguidist.sections.js
@@ -245,6 +245,14 @@ module.exports = {
             'SideNavigationItem/SideNavigationItem',
           ]),
         },
+        {
+          name: 'ActionMenu',
+          components: getComponentWithVariants('ActionMenu')([
+            'ActionMenu/ActionMenu',
+            'ActionMenuItem/ActionMenuItem',
+            'ActionMenuLink/ActionMenuLink',
+          ]),
+        },
       ],
       sectionDepth: 1,
       expand: true,
