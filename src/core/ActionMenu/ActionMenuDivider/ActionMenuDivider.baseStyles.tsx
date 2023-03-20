4459f92cda3aaf64c847df32561b1e97cdd55a85
Mikko Riippi <mikko.riippi@gofore.com>
2023-03-20 12:17:04 +0200

    Fix divider line on Safari

--- src/core/ActionMenu/ActionMenuDivider/ActionMenuDivider.baseStyles.tsx
+++ src/core/ActionMenu/ActionMenuDivider/ActionMenuDivider.baseStyles.tsx
@@ -4,32 +4,16 @@ import { SuomifiTheme } from '../../theme';
 export const baseStyles = (theme: SuomifiTheme) => css`
   &.fi-action-menu-divider {
     height: 17px;
-
     padding-left: 15px;
     padding-right: 15px;
-    padding-top: 8px;
-    padding-bottom: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
     .fi-action-menu-divider_line {
       height: 1px;
+      width: 100%;
       background-color: ${theme.colors.depthLight1};
-    }
-  }
-  &.fi-date-picker_application {
-    .fi-action-menu_divider {
-      background: ${theme.colors.depthLight1};
-      margin-top: ${theme.spacing.m};
-      margin-left: ${theme.spacing.m};
-      margin-right: ${theme.spacing.m};
-    }
-
-    &--small-screen {
-      background: ${theme.colors.highlightLight3};
-
-      .fi-service-action-menu_divider {
-        margin-top: ${theme.spacing.xxs};
-        margin-left: ${theme.spacing.s};
-        margin-right: ${theme.spacing.s};
-      }
     }
   }
 `;
