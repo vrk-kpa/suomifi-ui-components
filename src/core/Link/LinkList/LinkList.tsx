9aaf7a46ab96ce79634adc5249ca9ef64b98a6f4
Joonas Kaski <joonas.kaski@gofore.com>
2023-03-24 09:31:18 +0200

    remove built-in heading from link list

--- src/core/Link/LinkList/LinkList.tsx
+++ src/core/Link/LinkList/LinkList.tsx
@@ -3,19 +3,14 @@ import { default as styled } from 'styled-components';
 import classnames from 'classnames';
 import { LinkListStyles } from './LinkList.baseStyles';
 import { SuomifiThemeProp, SuomifiThemeConsumer } from '../../theme';
-import { hLevels, HtmlDiv, HtmlUlWithRef } from '../../../reset';
+import { HtmlUlWithRef } from '../../../reset';
 import { BaseLinkProps } from '../BaseLink/BaseLink';
-import { Heading } from '../../Heading/Heading';
 
 const LinkListClassName = 'fi-link-list';
 
 export interface LinkListProps extends BaseLinkProps {
   /** Ref  is passed to the anchor element. Alternative to React `ref` attribute. */
   forwardedRef?: React.Ref<HTMLAnchorElement>;
-  /** Heading element content */
-  headingText: string;
-  /** Semantic heading level. Look will always match h5 styles */
-  headingLevel?: hLevels;
 }
 
 const StyledLinkList = styled(
@@ -24,21 +19,14 @@ const StyledLinkList = styled(
     className,
     theme,
     children,
-    headingText,
-    headingLevel,
     ...passProps
   }: LinkListProps & SuomifiThemeProp) => (
-    <HtmlDiv>
-      <Heading variant="h5" as={headingLevel}>
-        {headingText}
-      </Heading>
-      <HtmlUlWithRef
-        {...passProps}
-        className={classnames(className, LinkListClassName)}
-      >
-        {children}
-      </HtmlUlWithRef>
-    </HtmlDiv>
+    <HtmlUlWithRef
+      {...passProps}
+      className={classnames(className, LinkListClassName)}
+    >
+      {children}
+    </HtmlUlWithRef>
   ),
 )`
   ${() => LinkListStyles()}
@@ -49,7 +37,7 @@ const StyledLinkList = styled(
  * Used for adding a link
  */
 const LinkList = forwardRef(
-  (props: LinkListProps, ref: React.Ref<HTMLAnchorElement>) => (
+  (props: LinkListProps, ref: React.Ref<HTMLUListElement>) => (
     <SuomifiThemeConsumer>
       {({ suomifiTheme }) => (
         <StyledLinkList theme={suomifiTheme} forwardedRef={ref} {...props} />
