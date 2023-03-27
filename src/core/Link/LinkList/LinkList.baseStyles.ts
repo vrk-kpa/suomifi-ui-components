46809435d5afe5c71bfa02311deed93d6908196d
Joonas Kaski <joonas.kaski@gofore.com>
2023-03-27 13:53:43 +0300

    adjust styles

--- src/core/Link/LinkList/LinkList.baseStyles.ts
+++ src/core/Link/LinkList/LinkList.baseStyles.ts
@@ -2,7 +2,6 @@ import { css } from 'styled-components';
 
 export const LinkListStyles = () => css`
   &.fi-link-list {
-    list-style: none;
     padding: 0;
     margin: 5px 0 0 0;
   }
--- src/core/Link/ListLink/ListLink.baseStyles.ts
+++ src/core/Link/ListLink/ListLink.baseStyles.ts
@@ -4,15 +4,12 @@ import { css } from 'styled-components';
 export const ListLinkStyles = (theme: SuomifiTheme) => css`
   &.fi-list-link {
     list-style: none;
-    & .fi-link {
-      font-size: 16px;
-    }
+    padding: 0;
     & .fi-list-link_icon {
       margin-right: 2px;
       & .fi-icon {
-        vertical-align: middle;
         margin-left: -3px;
-        transform: translateY(-0.1em);
+        transform: translateY(0.1em);
         & .fi-icon-base-fill {
           fill: ${theme.colors.accentBase};
         }
