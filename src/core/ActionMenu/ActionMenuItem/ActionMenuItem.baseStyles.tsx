5d025729178d896c39f842cae9df21a460c2e716
Mikko Riippi <mikko.riippi@gofore.com>
2023-04-18 08:38:33 +0300

    Menu item styling cleanup

--- src/core/ActionMenu/ActionMenuItem/ActionMenuItem.baseStyles.tsx
+++ src/core/ActionMenu/ActionMenuItem/ActionMenuItem.baseStyles.tsx
@@ -12,112 +12,67 @@ export const baseStyles = (theme: SuomifiTheme) => css`
     align-items: center;
     position: relative;
 
-    &:hover,
-    &:active {
-      background: ${theme.colors.highlightBase};
-      cursor: pointer;
-      color: ${theme.colors.whiteBase};
-    }
-
     .fi-link--router {
       ${font(theme)('bodyTextSmall')};
       text-decoration: none;
       color: ${theme.colors.blackBase};
-      display: flex;
       padding: ${theme.spacing.xs} ${theme.spacing.m};
+      display: flex;
       flex: 1;
       cursor: pointer;
       background: inherit;
       border: none;
       align-items: center;
 
-      &:hover,
-      &:active {
-        color: ${theme.colors.whiteBase};
-      }
-
+      &:active,
       &:focus,
+      &:hover,
       &:visited {
         color: ${theme.colors.blackBase};
       }
 
-      & > .fi-action-menu-item_icon {
+      .fi-action-menu-item_icon {
+        margin-right: ${theme.spacing.insetM};
         width: 16px;
         height: 16px;
-        margin-right: ${theme.spacing.insetM};
-
-        &.fi-action-menu-item_icon--right {
-          margin-right: 0;
-          margin-left: ${theme.spacing.insetM};
-        }
       }
     }
 
     &--disabled {
-      &:hover,
-      &:active {
-        background: transparent;
-        cursor: not-allowed;
-        .fi-link--router {
-          padding-left: ${theme.spacing.m};
-          color: ${theme.colors.depthBase};
-        }
-      }
       .fi-link--router {
         color: ${theme.colors.depthBase};
         cursor: not-allowed;
         pointer-events: none;
-        align-items: center;
 
-        &:hover,
         &:active,
+        &:focus,
+        &:hover,
         &:visited {
           color: ${theme.colors.depthBase};
         }
 
         .fi-action-menu-item_icon {
           cursor: not-allowed;
-          margin-right: ${theme.spacing.insetM};
         }
       }
     }
 
     &--selected {
-      display: flex;
-      align-items: center;
-      background: ${theme.colors.highlightBase};
-      color: ${theme.colors.whiteBase};
-      position: relative;
-      ${font(theme)('bodyTextSmall')};
-
       .fi-link--router {
-        font-weight: bold;
-        text-decoration: none;
         color: ${theme.colors.whiteBase};
-        display: flex;
-        padding: ${theme.spacing.xs} ${theme.spacing.m};
-        padding-left: ${theme.spacing.m};
-        width: 100%;
         background: ${theme.colors.highlightBase};
-        border: none;
-        align-items: center;
 
-        ${font(theme)('bodyTextSmall')};
-
-        &:hover,
         &:active,
+        &:focus,
+        &:hover,
         &:visited {
           text-decoration: none;
           color: ${theme.colors.whiteBase};
           cursor: pointer;
         }
 
-        .fi-action-menu-item_icon {
-          margin-right: ${theme.spacing.insetM};
-        }
-
         @media (forced-colors: active) {
-          /*  background-color: Highlight; */
+          background-color: Highlight;
         }
       }
     }
