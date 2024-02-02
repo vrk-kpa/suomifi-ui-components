240de432cef66d4e406a4f18695bfcdb6154065b
Joonas Kaski <joonas.kaski@gofore.com>
2024-02-02 14:48:02 +0200

    implement spacing consumer to status text and textarea

--- src/core/Form/StatusText/StatusText.baseStyles.tsx
+++ src/core/Form/StatusText/StatusText.baseStyles.tsx
@@ -1,12 +1,14 @@
 import { css } from 'styled-components';
 import { SuomifiTheme } from '../../theme';
 import { font } from '../../theme/reset';
+import { MarginProps, getCssMargins } from '../../theme/utils/spacing';
 
-export const baseStyles = (theme: SuomifiTheme) => css`
+export const baseStyles = (theme: SuomifiTheme, margins?: MarginProps) => css`
   ${font(theme)('bodySemiBoldSmall')};
   color: ${theme.colors.blackBase};
   font-size: 14px;
   line-height: 20px;
+  ${getCssMargins(margins)};
 
   &.fi-status-text {
     display: block;
