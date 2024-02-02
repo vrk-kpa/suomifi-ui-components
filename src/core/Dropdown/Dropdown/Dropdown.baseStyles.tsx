64b74b8803fccc172568d4f182404fb60679b7e3
Joonas Kaski <joonas.kaski@gofore.com>
2024-02-02 14:47:38 +0200

    implement spacing consumer to dropdown and singleselect

--- src/core/Dropdown/Dropdown/Dropdown.baseStyles.tsx
+++ src/core/Dropdown/Dropdown/Dropdown.baseStyles.tsx
@@ -1,9 +1,11 @@
 import { css } from 'styled-components';
 import { SuomifiTheme } from '../../theme';
 import { element, input } from '../../theme/reset';
+import { MarginProps, getCssMargins } from '../../theme/utils/spacing';
 
-export const baseStyles = (theme: SuomifiTheme) => css`
+export const baseStyles = (theme: SuomifiTheme, margins?: MarginProps) => css`
   width: 290px;
+  ${getCssMargins(margins)}
   .fi-dropdown_item-list {
     padding-top: 0;
   }
--- src/core/Form/Select/SingleSelect/SingleSelect.baseStyles.tsx
+++ src/core/Form/Select/SingleSelect/SingleSelect.baseStyles.tsx
@@ -1,9 +1,11 @@
 import { css } from 'styled-components';
 import { SuomifiTheme } from '../../../theme';
 import { font } from '../../../theme/reset';
+import { MarginProps, getCssMargins } from '../../../theme/utils/spacing';
 
-export const baseStyles = (theme: SuomifiTheme) => css`
+export const baseStyles = (theme: SuomifiTheme, margins?: MarginProps) => css`
   ${font(theme)('bodyText')}
+  ${getCssMargins(margins)}
   width: 290px;
 
   &.fi-single-select {
