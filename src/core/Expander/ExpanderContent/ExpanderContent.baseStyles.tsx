41fdfb390e31381a8ee42f4156a02c3c771ec5a4
Joonas Kaski <joonas.kaski@gofore.com>
2024-09-18 14:39:32 +0300

    remove broken expander animation

--- src/core/Expander/ExpanderContent/ExpanderContent.baseStyles.tsx
+++ src/core/Expander/ExpanderContent/ExpanderContent.baseStyles.tsx
@@ -34,20 +34,8 @@ export const baseStyles = (theme: SuomifiTheme) => css`
     border-top: 1px solid transparent;
     border-top-left-radius: 0;
     border-top-right-radius: 0;
-    /* This is very robust - cannot animate dynamic height with height-definition */
-    animation: fi-expander_content-anim ${theme.transitions.basicTime}
-      ${theme.transitions.basicTimingFunction} 1 forwards;
     &:not(.fi-expander_content--no-padding) {
       padding: ${theme.spacing.xs} ${theme.spacing.m} ${theme.spacing.m};
-    }
-  }
-  @keyframes fi-expander_content-anim {
-    0% {
-      height: auto;
-      transform: scaleY(0);
-    }
-    100% {
-      transform: scaleY(1);
     }
   }
 `;
