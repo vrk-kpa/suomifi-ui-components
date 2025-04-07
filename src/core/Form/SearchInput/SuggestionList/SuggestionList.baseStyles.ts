a6d7f30b57b3ca88b4d582d561eeea16f19c9310
Joonas Kaski <joonas.kaski@gofore.com>
2025-04-07 13:40:54 +0300

    replace SelectItemList with new SuggestionList

---
+++ src/core/Form/SearchInput/SuggestionList/SuggestionList.baseStyles.ts
@@ -0,0 +1,27 @@
+import { css } from 'styled-components';
+import { SuomifiTheme } from '../../../theme';
+import { font } from '../../../theme/reset';
+
+export const baseStyles = (theme: SuomifiTheme) => css`
+  ${font(theme)('bodyText')}
+  list-style-type: none;
+  box-sizing: border-box;
+  max-height: 265px;
+  background-color: ${theme.colors.whiteBase};
+  border-width: 0 1px 1px 1px;
+  border-style: solid;
+  border-color: ${theme.colors.depthDark3};
+  border-bottom-left-radius: ${theme.radiuses.basic};
+  border-bottom-right-radius: ${theme.radiuses.basic};
+  margin: 0;
+  padding: 4px 0 0 0;
+
+  display: block;
+  width: calc(100% + 26px);
+  overflow-y: auto;
+  overflow-x: hidden;
+  margin-left: -1px;
+  &:focus {
+    outline: none;
+  }
+`;
