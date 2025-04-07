b0b615946c6952dced8bcc6fcee3c393d9c3b8c5
Joonas Kaski <joonas.kaski@gofore.com>
2025-04-07 13:41:49 +0300

    remove redundant styles from searchinput

--- src/core/Form/SearchInput/SearchInput.baseStyles.tsx
+++ src/core/Form/SearchInput/SearchInput.baseStyles.tsx
@@ -219,28 +219,5 @@ export const baseStyles = (
         }
       }
     }
-    &.fi-search-input_suggestions {
-      ${font(theme)('bodyText')}
-      list-style-type: none;
-      box-sizing: border-box;
-      max-height: 265px;
-      background-color: ${theme.colors.whiteBase};
-      border-width: 0 1px 1px 1px;
-      border-style: solid;
-      border-color: ${theme.colors.depthDark3};
-      border-bottom-left-radius: ${theme.radiuses.basic};
-      border-bottom-right-radius: ${theme.radiuses.basic};
-      margin: 0;
-      padding: 4px 0 0 0;
-
-      display: block;
-      width: 100%;
-      overflow-y: auto;
-      overflow-x: hidden;
-
-      &:focus {
-        outline: none;
-      }
-    }
   }
 `;
