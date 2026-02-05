72a6562cbd1d7ad786e8053fbffd888bc924f589
Joonas Kaski <joonas.kaski@gofore.com>
2026-02-05 14:48:00 +0200

    optimize styleguidist build

--- .styleguidist/webpack.config.js
+++ .styleguidist/webpack.config.js
@@ -3,7 +3,38 @@ const webpack = require('webpack');
 
 module.exports = (env) => ({
   mode: env.production ? 'production' : 'development',
-  devtool: env.production ? 'inline-source-map' : 'eval',
+  devtool: env.production ? 'source-map' : 'eval',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: 10,
+      minSize: 30000,
+      cacheGroups: {
+        reactVendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-is)[\\/]/,
+          name: 'vendor.react',
+          priority: 20,
+        },
+        styleguidistVendor: {
+          test: /[\\/]node_modules[\\/](react-styleguidist|buble)[\\/]/,
+          name: 'vendor.styleguidist',
+          priority: 15,
+        },
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor.libs',
+          priority: 10,
+        },
+      },
+    },
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     fallback: {
